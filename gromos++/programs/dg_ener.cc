d36911c7ab69be865882e283c9b5d5d8c27c8595
Old Commits <chris.oostenbrink@boku.ac.at>
2002-01-24 12:44:26 +0000

    including pair correction

--- gromos++/programs/dg_ener.cc
+++ gromos++/programs/dg_ener.cc
@@ -1,5 +1,7 @@
 // dg_ener.cc This program calculates a free energy difference by reading
 //            in several output files of ener, for state A and B
+//            and correcting for the double counting of pairenergies
+//            from the output of pairener
 
 #include "../src/args/Arguments.h"
 #include <fstream>
@@ -13,21 +15,26 @@ using namespace args;
 
 int main(int argc, char **argv){
 
-  char *knowns[] = {"nfiles", "stateA", "stateB"};
-  int nknowns = 3;
+  char *knowns[] = {"nfiles", "stateA", "pairA", "stateB", "pairB"};
+  int nknowns = 5;
 
   string usage = argv[0];
   usage += "\n\t@nfiles <total number of files (A+B)\n";
   usage += "\t@stateA <energy files for state A>\n";
-  usage += "\t@atateB <energy files for state B>\n";
+  usage += "\t@pairA  <pair energy for atoms included in A>\n";
+  usage += "\t@stateB <energy files for state B>\n";
+  usage += "\t@pairB  <pair energy for atoms included in B>\n";
+  
 
   try{
     Arguments args(argc, argv, nknowns, knowns, usage);
 
+
     // set some values
-    int nfiles=0, na=0, nb=0;
+    int nfiles=0, na=0, nca=0, nb =0, ncb=0;
     double kT = 0.3*8.31441;
     
+    args.check("nfiles", 1);
     
     {
       Arguments::const_iterator iter=args.lower_bound("nfiles");
@@ -44,16 +51,31 @@ int main(int argc, char **argv){
       files[na].open((iter->second).c_str());
       na++;
     }
+    for(Arguments::const_iterator
+        iter=args.lower_bound("pairA"),
+        to=args.upper_bound("pairA");
+        iter!=to; ++iter){      
+      files[na+nca].open((iter->second).c_str());
+      nca++;
+    }
+    
     for(Arguments::const_iterator 
         iter=args.lower_bound("stateB"),
         to=args.upper_bound("stateB");
         iter!=to; ++iter){
-      files[na+nb].open((iter->second).c_str());
+      files[na+nca+nb].open((iter->second).c_str());
       nb++;
+    }
+    for(Arguments::const_iterator
+        iter=args.lower_bound("pairB"),
+        to=args.upper_bound("pairB");
+        iter!=to; ++iter){      
+      files[na+nca+nb].open((iter->second).c_str());
+      ncb++;
     }
 
     double time;
-    double ea[6], eb[6], fdum, dh, dg, p, sum=0, ave;
+    double ea[6], eb[6], eca[2], ecb[2], fdum, dh, dg, p, sum=0, ave;
     int cont=1, num=1;
     
 
@@ -62,18 +84,26 @@ int main(int argc, char **argv){
       if((files[i] >> time)==0) cont=0;
     while(cont){
       for(int j=0;j<6;j++){ ea[j]=0.0; eb[j]=0.0; }
-      
+      // read energies for state A
       for(int i=0;i<na;i++)
         for(int j=0;j<6;j++){
           files[i] >> fdum;
           ea[j]+=fdum;
 	}
-      for(int i=na;i<nfiles;i++)
+      // read pair corrections for state A
+      for(int i=0;i<nca;i++)
+        files[na+i] >> eca[0] >> eca[1];
+      // read energies for state B
+      for(int i=0;i<nb;i++)
         for(int j=0;j<6;j++){
-          files[i] >> fdum;
+          files[na+nca+i] >> fdum;
 	  eb[j]+=fdum;
 	}
-      dh = eb[4]+eb[5]-ea[4]-ea[5];
+      //read pair corrections for state B
+      for(int i=0;i<ncb;i++)
+        files[na+nca+nb+i] >> ecb[0] >> ecb[1];
+
+      dh = eb[4]-ecb[0]+eb[5]-ecb[1]-ea[4]+eca[0]-ea[5]+eca[1];
       p = exp(-dh/kT);
       sum +=p;
       ave = sum/num;
