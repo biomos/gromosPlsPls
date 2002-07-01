cbccd39b6808309c913f8384e6a8c20abc44417f
Old Commits <chris.oostenbrink@boku.ac.at>
2002-07-01 13:23:14 +0000

    fixed operator< to allow for more impropers with the same atoms --chris

--- gromos++/src/gcore/Improper.cc
+++ gromos++/src/gcore/Improper.cc
@@ -40,6 +40,6 @@ Improper &Improper::operator=(const Improper &b){
 int gcore::operator<(const Improper &a, const Improper &b){
 return (a[0]<b[0]||(a[0]==b[0]&&a[1]<b[1])
 	||(a[0]==b[0]&&a[1]==b[1]&&a[2]<b[2])
-	||(a[0]==b[0]&&a[1]==b[1]&&a[2]==b[2]
-	   &&a[3]<b[3]));
+	||(a[0]==b[0]&&a[1]==b[1]&&a[2]==b[2]&&a[3]<b[3])
+	||(a[0]==b[0]&&a[1]==b[1]&&a[2]==b[2]&&a[3]==b[3]&&a.type()<b.type()));
 }
