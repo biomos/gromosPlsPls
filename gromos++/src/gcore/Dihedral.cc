9b7ced9a4fa084a8d583a6daa60ddad07e5f23b6
Old Commits <chris.oostenbrink@boku.ac.at>
2002-07-01 13:23:00 +0000

    fixed operator< to allow for more dihedrals with the same atoms --chris

--- gromos++/src/gcore/Dihedral.cc
+++ gromos++/src/gcore/Dihedral.cc
@@ -40,6 +40,6 @@ Dihedral &Dihedral::operator=(const Dihedral &b){
 int gcore::operator<(const Dihedral &a, const Dihedral &b){
   return (a[1]<b[1]||(a[1]==b[1]&&a[2]<b[2])
 	  ||(a[1]==b[1]&&a[2]==b[2]&&a[0]<b[0])
-	  ||(a[1]==b[1]&&a[2]==b[2]&&a[0]==b[0]
-	     &&a[3]<b[3]));
+	  ||(a[1]==b[1]&&a[2]==b[2]&&a[0]==b[0]&&a[3]<b[3])
+	  ||(a[1]==b[1]&&a[2]==b[2]&&a[0]==b[0]&&a[3]==b[3]&&a.type()<b.type()));
 }
