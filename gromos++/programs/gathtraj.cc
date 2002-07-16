84b56d5b6c6ef5d0b896b11a9597067c4269d035
Old Commits <chris.oostenbrink@boku.ac.at>
2002-07-16 15:38:31 +0000

    GatherParse update --mika

--- gromos++/programs/gathtraj.cc
+++ gromos++/programs/gathtraj.cc
@@ -5,6 +5,7 @@
 
 #include "../src/args/Arguments.h"
 #include "../src/args/BoundaryParser.h"
+#include "../src/args/GatherParser.h"
 #include "../src/gio/InG96.h"
 #include "../src/gio/OutG96.h"
 #include "../src/gcore/System.h"
@@ -24,27 +25,16 @@ using namespace gio;
 using namespace bound;
 using namespace args;
 
-// Wrappers for the gathering methods.
-void gather(Boundary *b, gmath::Vec dummy){
-  b->gather();
-}
-void gathergr(Boundary *b, gmath::Vec dummy){
-  b->gathergr();
-}
-void coggather(Boundary *b, gmath::Vec centerOfGravity){
-  b->coggather(centerOfGravity);
-}
+
 
 int main(int argc, char **argv){
 
-  char *knowns[] = {"topo", "pbc", "gath", "cog", "traj"};
-  int nknowns = 5;
+  char *knowns[] = {"topo", "pbc", "traj"};
+  int nknowns = 3;
 
   string usage = argv[0];
   usage += "\n\t@topo <topology> (defaults to \"topo\")\n";
   usage += "\t@pbc <boundary type> (defaults to \"t\")\n";
-  usage += "\t@gath <gathering method> (defaults to \"gather\")\n";
-  usage += "\t@cog <centering vector> (ignored when gath != \"coggather\")\n";
   usage += "\t@traj <trajectory files> (defaults to \"traj\")\n";
  
   try{
@@ -70,32 +60,9 @@ int main(int argc, char **argv){
     catch (const gromos::Exception &e){
       pbc = new TruncOct(&sys);
     }
+// GatherParser
+    Boundary::MemPtr gathmethod = args::GatherParser::parse(args);
 
-    // check gathering method
-    gmath::Vec centeringVector;
-    void (*gatherMethod)(bound::Boundary *, gmath::Vec cVector) = NULL;
-    string gath;
-    try{
-      args.check("gath", 1);
-      gath = args["gath"].c_str();
-    }
-    catch (const gromos::Exception &e){ 
-      gath = "gather";
-    }
-    if(gath == "gather")
-      gatherMethod = gather;
-    else if(gath == "gathergr")
-      gatherMethod = gathergr;
-    else if(gath == "coggather"){
-      gatherMethod = coggather;
-      // fill in the coordinates of the cog-vector
-      args.check("cog", 3);
-      Arguments::const_iterator coord = args.lower_bound("cog");
-      for(int i = 0; i < 3; i++){
-        centeringVector[i] = atof(coord->second.c_str());
-        coord++;
-      }
-    }
     
     // define input coordinate
     InG96 ic;
@@ -115,7 +82,7 @@ int main(int argc, char **argv){
     // loop over single trajectory
     while(!ic.eof()){
       ic >> sys;
-      gatherMethod(pbc, centeringVector);
+     (*pbc.*gathmethod)();
       *oc << sys;
     }
 
