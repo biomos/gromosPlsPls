a7c771fe461391f59ad51c68a58a4bea3014e4e6
Old Commits <chris.oostenbrink@boku.ac.at>
2002-10-04 09:47:36 +0000

    Needed to generate the documentation

---
+++ gromos++/src/Doxygen.h
@@ -0,0 +1,58 @@
+/**
+ * The namespace gcore contains all of the most important
+ * classes that contain the information of your system.
+ *
+ * @defgroup gcore gcore
+ */
+
+
+/**
+ * The namespace gio defines all the in- and output
+ * streams that can be used to read in gromos96 files
+ * and produce formatted gromos96 files.
+ *
+ * @defgroup gio gio
+ */
+
+/**
+ * The namespace args contains the functionality that is needed 
+ * for the argument reading from the command line.
+ * Also, there are several parsers for input parameters that are 
+ * very often used.
+ *
+ * @defgroup args args
+ */
+
+/**
+ * The namespace bound contains the code for a correct treatment of
+ * periodic boundary conditions
+ * 
+ * @defgroup bound bound
+ */
+
+/** 
+ * The namespace fit contains code for rotational and translational
+ * fitting of systems
+ *
+ * @defgroup fit fit
+ */
+
+/**
+ * The namespace gmath contains gromos++ specific mathematical functions
+ *
+ * @defgroup gmath gmath
+ */
+
+/**
+ * The namespace gromos contains basically only the gromos++ Exception (?)
+ *
+ * @defgroup gromos gromos
+ */
+
+/** 
+ * The namespace utils contains several topics that where not clearly 
+ * belonging anywhere else. Also additional files for some programs can
+ * be found in the utils directory.
+ *
+ * @defgroup utils utils
+ */
