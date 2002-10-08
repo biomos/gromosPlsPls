c06c96e6a8dc4705a1282a58dada98820c628e35
Old Commits <chris.oostenbrink@boku.ac.at>
2002-10-08 09:15:40 +0000

    did some documentation --chris

--- gromos++/src/gio/OutTopology.h
+++ gromos++/src/gio/OutTopology.h
@@ -12,6 +12,18 @@ namespace gcore{
 }
 
 namespace gio{
+  /**
+   * Class OutTopology
+   * an outstream that defines how a gromos96 topology should be written out
+   * 
+   * The outputted topology does not have all the comments that a gromos96
+   * topology has, but it can be read by GROMOS96 without problem
+   *
+   * @class OutTopology
+   * @author B.C. Oostenbrink (actually, I am not sure)
+   * @ingroup gio
+   * @todo finish documentation
+   */
   class OutTopology{
     std::string d_title;
     std::ostream &d_os;
