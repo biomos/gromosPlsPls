45eb2205d58e05572ab8a812ff2fc2a29254db3e
Old Commits <chris.oostenbrink@boku.ac.at>
2003-05-27 08:33:13 +0000

    new version that does everything

--- gromos++/programs/redtop.cc
+++ gromos++/programs/redtop.cc
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <sstream>
+#include <set>
+#include <map>
+#include <vector>
 #include "../src/args/Arguments.h"
 #include "../src/gio/InTopology.h"
 #include "../src/gio/OutTopology.h"
@@ -21,145 +24,90 @@
 #include "../src/gcore/Solvent.h"
 #include "../src/gcore/SolventTopology.h"
 #include "../src/gcore/System.h"
+#include "../src/gcore/BbSolute.h"
+#include "../src/gcore/BuildingBlock.h"
 #include "../src/utils/AtomSpecifier.h"
+
+
 
 using namespace gcore;
 using namespace gio;
 using namespace args;
 
+// we use functionality of maketop
+#include "../src/utils/maketop.h"
+
+void linearizeTopology(vector<AtomTopology> &atoms, 
+		       vector<Bond> &bonds,
+		       vector<Angle> &angle,
+		       vector<Improper> &imp,
+		       vector<Dihedral> &dih,
+		       map<int, int> &resnum,
+		       vector<string> &resname,
+		       const System &sys, 
+		       utils::AtomSpecifier &keep);
 
 int main(int argc, char *argv[]){
 
-  char *knowns[] = {"topo", "mol"};
+  char *knowns[] = {"topo", "atoms"};
   int nknowns = 2;
   
   string usage = argv[0];
   usage += "\n\t@topo <topology>\n";
-  usage += "\t@mol <mol>[:<first atom>-<last atom>]\n";
+  usage += "\t@atoms <atoms to keep>\n";
 
   try{
     Arguments args(argc, argv, nknowns, knowns, usage);
 
     InTopology it(args["topo"]);
     System sys(it.system());
-    System syo;
-    ostringstream addtitle;
-
     
-    addtitle << "\nREDTOP molecules: ";
-    
-    // Parse atom specifiers, loop over all molecules to consider
-    for(Arguments::const_iterator iter=args.lower_bound("mol"), 
-          to=args.upper_bound("mol"); iter!=to;++iter) {
-      string s=iter->second.c_str();
-      utils::AtomSpecifier atspec(sys,s);
-      
-      int atoff=atspec.atom(0);
-      int atend=atspec.atom(atspec.size()-1)+1;
-      int mol=atspec.mol(0);
-      
-
-      MoleculeTopology mti=sys.mol(mol).topology();
-      MoleculeTopology mto;
-
-      AtomTopology ato;
-  
-      // First, do the atoms
-      int lastres=-1, currres=-1;
-      
-      for(int i=atoff; i<atend; i++){
-        Exclusion tempex, tempex14;
-        
-        ato.setName(mti.atom(i).name());
-        ato.setIac(mti.atom(i).iac());
-        ato.setChargeGroup(mti.atom(i).chargeGroup());
-        ato.setMass(mti.atom(i).mass());
-        ato.setCharge(mti.atom(i).charge());
-
-        int num=mti.atom(i).exclusion().size();
-        int num14=mti.atom(i).exclusion14().size();
-        for(int k=0; k<num; ++k){
-          int ati=mti.atom(i).exclusion().atom(k);
-          if(ati<atend)
-            tempex.insert(ati-atoff);
-        }
-        for(int k=0; k<num14; ++k){
-          int ati14=mti.atom(i).exclusion14().atom(k);
-          if(ati14<atend)
-            tempex14.insert(ati14-atoff);
-        }
-      
-        ato.setExclusion(tempex);
-        ato.setExclusion14(tempex14);
-
-        mto.addAtom(ato);
-        if(mti.resNum(i)!=lastres){
-	  lastres=mti.resNum(i);
-	  currres++;
-          mto.setResName(currres, mti.resName(mti.resNum(i)));
-	}
-	
-        mto.setResNum(i-atoff,currres);
-      }
-
-      // Now, for the bonds, angles etc.
-      BondIterator bi(mti);
-      for(;bi;++bi){
-        if(bi()[0]>=atoff&&bi()[0]<atend&&
-            bi()[1]>=atoff&&bi()[1]<atend){
-          Bond b(bi()[0]-atoff,bi()[1]-atoff);
-          b.setType(bi().type());
-          mto.addBond(b);
-        }
-      }
-  
-      AngleIterator ai(mti);
-      for(;ai;++ai){
-        if(ai()[0]>=atoff&&ai()[0]<atend&&
-           ai()[1]>=atoff&&ai()[1]<atend&&
-           ai()[2]>=atoff&&ai()[2]<atend){
-          Angle a(ai()[0]-atoff,ai()[1]-atoff,ai()[2]-atoff);
-          a.setType(ai().type());
-          mto.addAngle(a);
-        }
-      }
-      ImproperIterator ii(mti);
-      for(;ii;++ii){
-        if(ii()[0]>=atoff&&ii()[0]<atend&&
-           ii()[1]>=atoff&&ii()[1]<atend&&
-           ii()[2]>=atoff&&ii()[2]<atend&&
-           ii()[3]>=atoff&&ii()[3]<atend){
-          Improper imp(ii()[0]-atoff, ii()[1]-atoff,
-                       ii()[2]-atoff, ii()[3]-atoff);
-          imp.setType(ii().type());
-          mto.addImproper(imp);
-        }
-      }
-      DihedralIterator di(mti);
-      for(;di;++di){
-        if(di()[0]>=atoff&&di()[0]<atend&&
-           di()[1]>=atoff&&di()[1]<atend&&
-           di()[2]>=atoff&&di()[2]<atend&&
-           di()[3]>=atoff&&di()[3]<atend){
-          Dihedral dih(di()[0]-atoff, di()[1]-atoff,
-                       di()[2]-atoff, di()[3]-atoff);
-          dih.setType(di().type());
-          mto.addDihedral(dih);
-        }
-      }
- 
-      syo.addMolecule(mto);
-      addtitle << mol+1 << ":" << atoff+1 << "-" << atend << " ";
+    // Parse atom specifiers
+    utils::AtomSpecifier as(sys);
+    for(Arguments::const_iterator iter=args.lower_bound("atoms"), 
+          to=args.upper_bound("atoms"); iter!=to;++iter) {
+      as.addSpecifier(iter->second);
     }
+    vector<string> as_str = as.toString();
+ 
+    // linearize the topology
+    // flag all atoms that are to be removed with a negative iac
+    vector<AtomTopology> atoms;
+    vector<string> resNames;
+    vector<Bond> bonds;
+    vector<Angle> angles;
+    vector<Improper> imps;
+    vector<Dihedral> dihs;
+    map<int, int> resMap;
+
+    linearizeTopology(atoms, bonds, angles, imps, dihs, 
+		      resMap, resNames, sys, as);
+
+    // now call the function removeAtom from maketop
+    removeAtoms(&atoms, &bonds, &angles, &imps, &dihs, &resMap, &resNames);
+ 
+    // calculate the new 1,4 interactions
+    get14s(&atoms, &bonds);
     
+    // and parse the linearized thing back into a topology (which might
+    // have a zillion molecules, because bonds have been removed)
+    System syo = parseTopology(&atoms, &bonds, &angles, &imps, &dihs, 
+			       &resNames, &resMap);
+    
+    // take the old solvent
     syo.addSolvent(sys.sol(0));
     
-    
+    // and write out the new topology
     OutTopology ot(cout);
-    addtitle << "\nfrom: " << args["topo"];
-    ot.setTitle(it.title()+addtitle.str());
-
-    ot.write(syo,it.forceField());
+    ostringstream os;
+    os << "Reduced topology based on " << args["topo"] << endl;
+    os << "using atoms ";
+    for(unsigned int i=0; i< as_str.size(); i++)
+      os << as_str[i] << " ";
+    
+    ot.setTitle(os.str());
+    
+    ot.write(syo, it.forceField());
     
     return 0;
   }
@@ -173,3 +121,57 @@ int main(int argc, char *argv[]){
 
 
 
+void linearizeTopology(vector<AtomTopology> &atom, 
+		       vector<Bond> &bond,
+		       vector<Angle> &angle,
+		       vector<Improper> &imp,
+		       vector<Dihedral> &dih,
+		       map<int, int> &resmap,
+		       vector<string> &resname,
+		       const System &sys,
+		       utils::AtomSpecifier &keep)
+{
+  int lastAtom=0;
+  int lastRes=0;
+
+  for(int m=0; m< sys.numMolecules(); m++){
+    for(int a=0; a<sys.mol(m).numAtoms(); a++){
+      atom.push_back(sys.mol(m).topology().atom(a));
+      resmap[a+lastAtom]=sys.mol(m).topology().resNum(a)+lastRes;
+      if(keep.findAtom(m,a) == -1) atom[a+lastAtom].setIac(-1);
+    }
+    for(int i=0; i<sys.mol(m).topology().numRes(); i++)
+      resname.push_back(sys.mol(m).topology().resName(i));
+      
+    BondIterator bi(sys.mol(m).topology());
+    for(; bi; ++bi){
+      Bond b=bi();
+      b[0]+=lastAtom; b[1]+=lastAtom;
+      bond.push_back(b);
+    }
+
+    AngleIterator ai(sys.mol(m).topology());
+    for(; ai; ++ai){
+      Angle a=ai();
+      a[0]+=lastAtom; a[1]+=lastAtom; a[2]+=lastAtom;
+      angle.push_back(a);
+    }
+    
+    DihedralIterator di(sys.mol(m).topology());
+    for(; di; ++di){
+      Dihedral d=di();
+      d[0]+=lastAtom; d[1]+=lastAtom; d[2]+=lastAtom; d[3]+=lastAtom;
+      dih.push_back(d);
+    }
+
+    ImproperIterator ii(sys.mol(m).topology());
+    for(; ii; ++ii){
+      Improper i=ii();
+      i[0]+=lastAtom; i[1]+=lastAtom; i[2]+=lastAtom; i[3]+=lastAtom;
+      imp.push_back(i);
+    }
+
+    lastRes+=sys.mol(m).topology().numRes();
+    lastAtom+=sys.mol(m).numAtoms();
+  }
+}
