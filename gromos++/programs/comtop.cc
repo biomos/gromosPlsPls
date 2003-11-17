857dc00bc288d627d19086db0d24b7c4020cb03f
Old Commits <chris.oostenbrink@boku.ac.at>
2003-11-17 10:40:13 +0000

    added number functionality --chris

--- gromos++/programs/comtop.cc
+++ gromos++/programs/comtop.cc
@@ -11,6 +11,7 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 using namespace gcore;
@@ -24,43 +25,64 @@ int main(int argc, char **argv){
   int nknowns = 3;
 
   string usage = argv[0];
-  usage += "\n\t@topo <topologies>\n";
+  usage += "\n\t@topo  <topologies>\n";
   usage += "\t@param <number of topology to take parameters from>\n";
-  usage += "\t@solv <number of topology to take solvent from>\n";
+  usage += "\t@solv  <number of topology to take solvent from>\n";
 
   try{
     Arguments args(argc, argv, nknowns, knowns, usage);
     // set some values
-    args.check("param",1);
+    int parnum=1;
+    if(args.count("param")>0) parnum=atoi(args["param"].c_str());
+    int solnum=1;
+    if(args.count("solv")>0)  solnum=atoi(args["solv"].c_str());
+    if(args.count("topo")<=0)
+      throw gromos::Exception("COMTOP", "needs at least one topology\n"+usage);
     
-    Arguments::const_iterator iter=args.lower_bound("param");
-    int parnum=atoi(iter->second.c_str());
-    iter=args.lower_bound("solv");
-    int solnum=atoi(iter->second.c_str());
-    int topnum=0;
-   
     System sys;
 
     //ugly solution to the not yet implemented '=' for force fields
-    string paramname;
+    string paramname, toponame, s;
+    std::string::size_type s_it;
+    int repeat = 1, topnum=0, oldtopnum=-1;
+    
     ostringstream title;
     title << "COMTOP: Combined topology using:\n";
     
        
     for(Arguments::const_iterator iter=args.lower_bound("topo"),
          to=args.upper_bound("topo"); iter!=to; ++iter){
-      topnum++;
+      oldtopnum=topnum+1;
+      
+      s=iter->second;
+      s_it=s.find(':');
+      
+      if(s_it == string::npos){
+	toponame=s;
+	repeat=1;
+      }
+      else{
+	toponame=s.substr(s_it+1,s.size());
+	repeat=atoi(s.substr(0,s_it).c_str());
+      }
+      topnum+=repeat;
       
       // read topology
-      InTopology it(iter->second.c_str());
-    
-      for(int j=0;j<it.system().numMolecules();j++)
-        sys.addMolecule(it.system().mol(j));
-      if(topnum==solnum)
-        sys.addSolvent(it.system().sol(0));
-      if(topnum==parnum)
-        paramname=iter->second.c_str();
-      title << topnum << ". " << iter->second.c_str() << endl;
+      InTopology it(toponame);
+      for(int i=0; i<repeat; i++)
+	for(int j=0;j<it.system().numMolecules();j++)
+	  sys.addMolecule(it.system().mol(j));
+
+      if(solnum <= topnum && solnum >= oldtopnum)
+	sys.addSolvent(it.system().sol(0));
+      if(parnum <= topnum && parnum >= oldtopnum)
+        paramname=toponame;
+      if(topnum!=oldtopnum)
+	title << setw(4) << oldtopnum << " .. " << setw(4) << topnum;
+      else
+	title << setw(12) << topnum;
+      
+      title << " : " << toponame << endl;
     }
     InTopology it(paramname);
     title << "Parameters from " << parnum 
