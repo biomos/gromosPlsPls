24839b1a697198d5c32e457f262c48f5bbb2aa2e
Old Commits <chris.oostenbrink@boku.ac.at>
2003-11-20 17:08:29 +0000

    added sorting of the atoms in which we are actually interested --chris

--- gromos++/programs/rmsf.cc
+++ gromos++/programs/rmsf.cc
@@ -78,127 +78,129 @@ int main(int argc, char **argv){
     ic.close();
 
 
-  // Parse boundary conditions
-  Boundary *pbc = BoundaryParser::boundary(refSys, args);
-  // parse gather method
-  Boundary::MemPtr gathmethod = args::GatherParser::parse(args);
-
-  // gather reference system
-  (*pbc.*gathmethod)();
-  delete pbc;
-
- 
-   Reference ref(&refSys);
-  //get the atoms for the fit
-  AtomSpecifier fitatoms(refSys);
-   {
-    Arguments::const_iterator iter = args.lower_bound("atomsfit");
-    Arguments::const_iterator to = args.upper_bound("atomsfit");
-
-    for(;iter!=to;iter++){
-     string spec=iter->second.c_str();
-     fitatoms.addSpecifier(spec);
-    }
-   }
-
-   ref.addAtomSpecifier(fitatoms);
-   
-   // System for calculation
-    System sys(refSys);
-   //get the atoms for the rmsf
-   AtomSpecifier rmsfatoms(sys); 
-   {
-    Arguments::const_iterator iter = args.lower_bound("atomsrmsf");
-    Arguments::const_iterator to = args.upper_bound("atomsrmsf");
-
-    for(;iter!=to;iter++){
-     string spec=iter->second.c_str();
-     rmsfatoms.addSpecifier(spec);
-    }
-   }
- 
-
+    // Parse boundary conditions
+    Boundary *pbc = BoundaryParser::boundary(refSys, args);
+    // parse gather method
+    Boundary::MemPtr gathmethod = args::GatherParser::parse(args);
     
-
-
+    // gather reference system
+    (*pbc.*gathmethod)();
+    delete pbc;
+    
+    
+    Reference ref(&refSys);
+    //get the atoms for the fit
+    AtomSpecifier fitatoms(refSys);
+    {
+      Arguments::const_iterator iter = args.lower_bound("atomsfit");
+      Arguments::const_iterator to = args.upper_bound("atomsfit");
+      
+      for(;iter!=to;iter++){
+	string spec=iter->second.c_str();
+	fitatoms.addSpecifier(spec);
+      }
+    }
+    
+    ref.addAtomSpecifier(fitatoms);
+    
+    // System for calculation
+    System sys(refSys);
+    //get the atoms for the rmsf
+    AtomSpecifier rmsfatoms(sys); 
+    {
+      Arguments::const_iterator iter = args.lower_bound("atomsrmsf");
+      Arguments::const_iterator to = args.upper_bound("atomsrmsf");
+      
+      for(;iter!=to;iter++){
+	string spec=iter->second.c_str();
+	rmsfatoms.addSpecifier(spec);
+      }
+    }
+    rmsfatoms.sort();
+    
+    
+    
+    
+    
     // Parse boundary conditions for sys
     pbc = BoundaryParser::boundary(sys, args);
     RotationalFit rf(&ref);
-
-
+    
+    
     int numFrames = 0;
- 
- //vectors to store the positions, average position and eventually the rmsf value
- vector<Vec> pos;
- vector<Vec> apos;
- vector<double> rmsf;
-
- //init apos for averaging
- Vec zero(0.0,0.0,0.0);
- Vec spos(0.0,0.0,0.0);
- for (int i=0; i < rmsfatoms.size(); ++i) {
+    
+    //vectors to store the positions, average position and eventually the rmsf value
+    vector<Vec> pos;
+    vector<Vec> apos;
+    vector<double> rmsf;
+    
+    //init apos for averaging
+    Vec zero(0.0,0.0,0.0);
+    Vec spos(0.0,0.0,0.0);
+    for (int i=0; i < rmsfatoms.size(); ++i) {
       apos.push_back(zero);
       rmsf.push_back(0.0);
- }
- 
- //loop over trajectory
- for(Arguments::const_iterator iter=args.lower_bound("traj");
-  iter!=args.upper_bound("traj"); ++iter){
-  ic.open(iter->second);
-    // loop over all frames
-  while(!ic.eof()){
-   numFrames++;
-   ic >> sys;
-
-    (*pbc.*gathmethod)();
-    rf.fit(&sys);
-        
-    
-    //push back the postion    
-    for (int i=0; i < rmsfatoms.size(); ++i) {
-     spos = *rmsfatoms.coord(i); 
-     pos.push_back(spos);
-     apos[i] += spos;
     }
-     
-        
+    
+    //loop over trajectory
+    for(Arguments::const_iterator iter=args.lower_bound("traj");
+	iter!=args.upper_bound("traj"); ++iter){
+      ic.open(iter->second);
+      // loop over all frames
+      while(!ic.eof()){
+	numFrames++;
+	ic >> sys;
+	
+	(*pbc.*gathmethod)();
+	rf.fit(&sys);
+	
+	
+	//push back the postion    
+	for (int i=0; i < rmsfatoms.size(); ++i) {
+	  spos = *rmsfatoms.coord(i); 
+	  pos.push_back(spos);
+	  apos[i] += spos;
+	}
+	
+	
+      }
+      ic.close();
+    } //end loop over trajectory
+    
+    
+    //average positions, write them out
+    for (int i=0; i < (int) apos.size(); ++i) apos[i] = apos[i]/numFrames;
+    
+    //calc rmsf
+    for (int i=0, j=0; i < (int) pos.size(); ++i) {
+      
+      Vec diff = pos[i] - apos[j];
+      rmsf[j] += diff.abs2();
+      
+      ++j;
+      
+      if (j == (int) rmsf.size())  j = 0;
+      
+    }
+    
+    //average
+    for (int i=0; i < (int) rmsf.size(); ++i)  rmsf[i] = rmsf[i]/numFrames;
+    
+    
+    //spit out results
+    
+    for (int i=0; i < rmsfatoms.size(); ++i) {
+      cout.precision(8);
+      cout << setw(5) << i+1
+	   << setw(14) << sqrt(rmsf[i])
+	   << "#" << setw(5) << rmsfatoms.name(i)
+	//sys.mol(rmsfatoms.mol(i)).topology().atom(rmsfatoms.atom(i)).name() 
+	   << endl;
+    }
+    
+    
   }
-  ic.close();
- } //end loop over trajectory
-   
-
-  //average positions, write them out
- for (int i=0; i < (int) apos.size(); ++i) apos[i] = apos[i]/numFrames;
- 
- //calc rmsf
- for (int i=0, j=0; i < (int) pos.size(); ++i) {
   
-   Vec diff = pos[i] - apos[j];
-   rmsf[j] += diff.abs2();
-   
-   ++j;
-   
-   if (j == (int) rmsf.size())  j = 0;
-
- }
-
- //average
- for (int i=0; i < (int) rmsf.size(); ++i)  rmsf[i] = rmsf[i]/numFrames;
-
-
- //spit out results
- 
- for (int i=0; i < rmsfatoms.size(); ++i) {
-   cout.precision(8);
-   cout << setw(5) << i+1
-        << setw(14) << sqrt(rmsf[i])
-        << setw(5) << sys.mol(rmsfatoms.mol(i)).topology().atom(rmsfatoms.atom(i)).name() 
-        << endl;
- }
-  
-
-  }
-
   catch (const gromos::Exception &e){
     cerr << e.what() << endl;
     exit(1);
