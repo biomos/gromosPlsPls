0dd2762512f6ca9c568e1254146d2723baedf54f
Old Commits <chris.oostenbrink@boku.ac.at>
2004-04-23 08:47:25 +0000

    first submission of new clustering programs --m&c

---
+++ gromos++/programs/postcluster.cc
@@ -0,0 +1,620 @@
+// A program to postprocess the output of the cluster program
+
+#include <cassert>
+#include <vector>
+#include <iomanip>
+#include <math.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <set>
+
+#include "../src/args/Arguments.h"
+#include "../src/args/BoundaryParser.h"
+#include "../src/args/GatherParser.h"
+#include "../src/fit/Reference.h"
+#include "../src/gio/InG96.h"
+#include "../src/gcore/System.h"
+#include "../src/gcore/Molecule.h"
+#include "../src/gcore/Box.h"
+#include "../src/gio/Ginstream.h"
+#include "../src/gio/InTopology.h"
+#include "../src/gio/OutG96.h"
+#include "../src/gio/OutG96S.h"
+#include "../src/bound/Boundary.h"
+#include "../src/gmath/Vec.h"
+
+using namespace std;
+using namespace gcore;
+using namespace gio;
+using namespace bound;
+using namespace args;
+using namespace utils;
+
+class StructureSpecifier: public set<int>
+{
+public:
+  void addSpecifier(string const s);
+protected:
+  void parse(string const s);
+};
+
+class cluster_parameter
+{
+public:
+  int num;
+  int maxstruct;
+  int skip;
+  int stride;
+  double t0;
+  double dt;
+  double cutoff;
+  bool free;
+  int precision;
+  int number_cluster;
+  cluster_parameter() : num(0), maxstruct(-1), skip(0), stride(1), 
+			t0(0.0), dt(1.0), cutoff(0.0),
+			free(true), precision(10000), number_cluster(0) {};
+};
+ 
+void read_structure_file(string const s, cluster_parameter & cp,
+		    vector< vector< int > > & cluster,
+		    vector< int > & cm);
+void read_ts_file(string const s, vector< int > &ts);
+void split_trajectory(Arguments const &args, StructureSpecifier const & cs,
+		      cluster_parameter const &cp, 
+		      vector<int> const & centralMember, 
+		      vector<int> const & timeSeries);
+void determine_lifetime(StructureSpecifier const &cs, 
+			cluster_parameter const &cp, 
+		        vector<int> const &timeSeries, 
+			int const lifeTimeLimit);
+
+void colour_structures(cluster_parameter const & cp, 
+		       vector<int> const & timeSeries, 
+		       vector<int> const & rgb);
+
+
+int main(int argc, char **argv){
+
+  char *knowns[] = {"topo", "cluster_struct", "cluster_ts", "clusters", 
+		    "lifetime", "traj", "rgb"};
+  int nknowns = 7;
+
+  string usage = argv[0];
+  usage += "\n\t@topo   <topology>\n";
+  usage += "\t@cluster_struct  <cluster_structures.dat>\n";
+  usage += "\t@cluster_ts      <cluster_ts>\n";
+  usage += "\t@clusters        <StructureSpecifier>\n";
+  usage += "\t[@lifetime       <lifetime limit>]\n";
+  usage += "\t[@rgb            <red> <green> <blue>]\n";
+  usage += "\t[@traj           <trajectory files>]\n";
+  
+ 
+  try{
+    Arguments args(argc, argv, nknowns, knowns, usage);
+    
+    // structure specifier
+    StructureSpecifier cs;
+    {
+      Arguments::const_iterator iter=args.lower_bound("clusters"),
+	to=args.upper_bound("clusters");
+      for(; iter!=to ; ++iter){
+	cs.addSpecifier(iter->second);
+      }
+    }
+
+    // read the lifeTimeLimit;
+    int lifeTimeLimit = -1;
+    if(args.count("lifetime")>0) 
+      lifeTimeLimit = atoi(args["lifetime"].c_str());
+
+    // read the rgb values;
+    vector<int> rgb;
+    {
+      Arguments::const_iterator iter=args.lower_bound("rgb"),
+	to=args.upper_bound("rgb");
+      int sum=0;
+      for(; iter!=to ; ++iter){
+	sum+=atoi(iter->second.c_str());
+	rgb.push_back(sum);
+      }
+    } 
+    
+    // read in the cluster_structures file
+    vector< vector< int > > cluster;
+    vector< int > timeSeries;
+    vector< int > centralMember;
+    cluster_parameter cp;
+
+    read_structure_file(args["cluster_struct"], cp, cluster, centralMember);
+    read_ts_file(args["cluster_ts"], timeSeries);
+    cout << "ts size " << timeSeries.size() << endl;
+    
+    if(rgb.size()){
+
+      colour_structures(cp, timeSeries, rgb);
+    }
+    
+    if(args.count("traj")>0){
+  
+      split_trajectory(args, cs, cp, centralMember, timeSeries);
+
+    }
+
+    if(lifeTimeLimit != -1){
+      
+      determine_lifetime(cs, cp, timeSeries, lifeTimeLimit);
+	
+    }
+    
+
+    
+
+    
+    
+  }
+  catch (const gromos::Exception &e){
+    cerr << "EXCEPTION:\t";
+    cerr << e.what() << endl;
+    exit(1);
+  }
+  return 0;
+}
+
+
+void StructureSpecifier::parse(string const s)
+{
+  std::string::size_type iterator;
+  if((iterator=s.find(',')) != std::string::npos){
+    parse(s.substr(0,iterator));
+    parse(s.substr(iterator+1, std::string::npos));
+  }
+  else{
+    istringstream is;
+    int rangeBegin, rangeEnd;
+    if((iterator=s.find('-')) != std::string::npos){
+      is.str(s.substr(0,iterator));
+      if(!(is >> rangeBegin))
+	throw gromos::Exception("StructureSpecifier", 
+				"Invalid begin of range "+ s);
+      is.clear();
+      is.str(s.substr(iterator+1, std::string::npos));
+      if(!(is >> rangeEnd))
+	throw gromos::Exception("StructureSpecifier", 
+				"Invalid end of range " + s);
+      for(int i=rangeBegin; i<= rangeEnd; ++i){
+	insert(i);
+      }
+    }
+    else{
+      is.str(s);
+      if(!(is >> rangeBegin))
+	throw gromos::Exception("StructureSpecifier", 
+				"Invalid structure specified "+ s);
+      insert(rangeBegin);
+    }
+  }
+}
+
+void StructureSpecifier::addSpecifier(string const s)
+{
+  parse(s);
+}
+
+     
+void read_structure_file(string const s, cluster_parameter & cp,
+			 vector< vector< int > > & cluster,
+			 vector< int > & cm)
+{
+  Ginstream gin(s);
+  
+  vector<string> buffer;
+  gin.getblock(buffer);
+  
+  if(buffer[0] != "CLUSTERPARAMETERS")
+    throw gromos::Exception("postcluster", "Could not read CLUSTERPARAMETERS "
+			    "from file " + s );
+  string b, e;
+  istringstream is(gio::concatenate(buffer.begin()+1, buffer.end(), b));
+  if(!(is >> cp.num >> cp.maxstruct >> cp.skip >> cp.stride
+       >> cp.t0 >> cp.dt >> cp.cutoff >> cp.free >> cp.precision
+       >> cp.number_cluster >> e))
+    throw gromos::Exception("postcluster", 
+			    "Error while reading CLUSTERPARAMETERS" );
+  if(e.substr(0,3)!="END")
+    throw gromos::Exception("postcluster",
+			    "Error while readind CLUSTERPARAMETERS; no END");
+  int p=cp.precision;
+  cp.precision=1;
+  for(int i=0; i< p; ++i){
+    cp.precision *= 10;
+  }
+
+  
+  gin.getblock(buffer);
+  if(buffer[0] != "CLUSTER")
+    throw gromos::Exception("postcluster", "Could not read CLUSTER "
+			    "from file " + s );
+
+  unsigned int numClusters=cp.number_cluster;
+  if(cp.free) numClusters++;
+  
+  cm.resize(numClusters);
+  string sdum;
+  unsigned int inum;
+  vector< int > csize(numClusters);
+  
+  for(unsigned int i=1; i< buffer.size()-1; ++i){
+    is.clear();
+    is.str(buffer[i]);
+    
+    if(!(is >> inum >> cm[i-1] >> sdum >> csize[i-1]))
+      throw gromos::Exception("postcluster", "Error while reading CLUSTER "
+			      "block. " + buffer[i]);
+    if(inum != i-1)
+      throw gromos::Exception("postcluster", "CLUSTER block is corrupted; "
+			      "wrong cluster number " + buffer[i]);
+    
+  }
+  if(buffer[buffer.size()-1].substr(0,3)!= "END")
+    throw gromos::Exception("postcluster", "CLUSTER block is corrupted; "
+			    "no END marker " + buffer[buffer.size()-1]);
+
+  gin.getblock(buffer);
+  if(buffer[0] != "MEMBERS")
+    throw gromos::Exception("postcluster", "Could not read MEMBERS "
+			    "from file " + s );
+  
+
+  is.clear();
+  is.str(gio::concatenate(buffer.begin()+1, buffer.end(), b));
+  cluster.resize(numClusters);
+  
+  for(unsigned int i=0; i< numClusters; ++i){
+    if(!(is >> inum) || i!=inum)
+      throw gromos::Exception("postcluster", "Error while reading MEMBERS; "
+			      "couldn't read cluster number");
+    cluster[i].resize(csize[i]);
+    
+    for(int j=0; j < csize[i]; ++j){
+      if(!(is >> cluster[i][j]))
+	throw gromos::Exception("postcluster", "Error while reading MEMBERS; "
+				"couldn't read a member");
+    }
+  }
+  if(!(is >> sdum) || sdum.substr(0,3) != "END")
+    throw gromos::Exception("postcluster",
+			    "Error while reading MEMBERS; no END " + sdum);
+  
+}
+
+  
+void read_ts_file(string const s, vector< int > &ts)
+{
+  ifstream fin(s.c_str());
+  string line;
+  std::string::size_type iterator;
+  int idum, iold = 0, cluster;
+  double fdum;
+  istringstream is;
+  
+  while(true){
+    std::getline(fin, line);
+    if(fin.eof()) break;
+    
+    if((iterator=line.find('#')) != std::string::npos){
+      if(iterator==0) continue;
+      line = line.substr(0,iterator);
+    }
+    
+    is.clear();
+    is.str(line);
+    if(!(is >> fdum >> idum >> cluster))
+      throw gromos::Exception("postcluster", "Error while reading time "
+			      "series file: " + line);
+    if(idum!=iold+1)
+      throw gromos::Exception("postcluster", "Error while reading time "
+			      "series file: structures not sequential");
+    
+    ts.push_back(cluster);
+    iold++;
+  }
+}
+
+
+void split_trajectory(Arguments const &args, StructureSpecifier const &cs,
+		      cluster_parameter const &cp, 
+		      vector<int> const &centralMember, 
+		      vector<int> const &timeSeries)
+{
+
+  //  read topology
+  InTopology it(args["topo"]);
+  System sys(it.system());
+
+
+  map<int, OutG96 *> file;
+  vector<ofstream *> ff(cs.size());
+
+  int count=0;
+  for(StructureSpecifier::const_iterator iter = cs.begin(), 
+	to=cs.end(); iter != to; ++iter, ++count){
+    ostringstream os, ot;
+    os << "cluster_" << *iter << ".trj";
+    ot << "Structures belonging to cluster " << *iter << "\n"
+       << "According to " << args["cluster_struct"] << "\n"
+       << "         and " << args["cluster_ts"];
+    ff[count] = new ofstream(os.str().c_str());
+	
+    file[*iter] = new OutG96(*ff[count]);
+    file[*iter]->select("ALL");
+    file[*iter]->writeTitle(ot.str());
+	
+    cout << *iter << endl;
+  }
+      
+  // define input stream
+  InG96 ic;
+  int frameNumber=0;
+  int frameCluster=0;
+      
+  for(Arguments::const_iterator iter=args.lower_bound("traj"), 
+	to=args.upper_bound("traj"); iter != to; ++iter){
+    ic.open(iter->second);
+    ic.select("ALL");
+	
+    while(!ic.eof()){
+      ic >> sys;
+	  
+      if(!((frameNumber - cp.skip) % cp.stride)){
+	    
+	if(cs.count(timeSeries[frameCluster])){
+	      
+	  (*file[ timeSeries[ frameCluster ] ]) << sys;
+	}  
+	// check whether it is a central member
+	if(centralMember[timeSeries[frameCluster]]== frameCluster+1){
+	  ostringstream os, ot;
+	  os << "cluster_" << timeSeries[frameCluster] << ".cms";
+	  ot << "Central member structure belonging to cluster " 
+	     << timeSeries[frameCluster] << "\n"
+	     << "According to " << args["cluster_struct"] << "\n"
+	     << "         and " << args["cluster_ts"];
+	  ofstream yaf(os.str().c_str());
+	  OutG96S og96(yaf);
+	  og96.select("ALL");
+	  og96.writeTitle(ot.str());
+	  og96 << sys;
+	  og96.close();
+	  yaf.close();
+	}
+	    
+	    
+	frameCluster++;
+      }
+	  
+      frameNumber++;
+	  
+	  
+    }
+  }
+      
+  // close the files
+  for(map<int, OutG96 *>::iterator iter=file.begin(),
+	to=file.end(); iter!=to; ++iter){
+    iter->second->close();
+    delete iter->second;
+  }
+  for(unsigned int i=0; i< ff.size(); ++i){
+    ff[i]->close();
+    delete ff[i];
+  }
+}
+
+
+
+void determine_lifetime(StructureSpecifier const &cs, 
+			cluster_parameter const &cp, 
+		        vector<int> const &timeSeries,
+			int const lifeTimeLimit)
+{
+  // the life time means that you
+  // 1. you only consider a cluster visited after so many hits
+  // 2. you consider less than that misses, as not happening.
+  //    if after such a short break, the old one continues for
+  //    at least the life time again, you treat that one as continuing.
+
+  int current = -1;
+  int numCluster=cp.number_cluster;
+  if(cp.free) numCluster++;
+  
+  vector<int> lifetime(numCluster, 0);
+  vector<int> occurence(numCluster, 0);
+  vector< vector < unsigned short > > pathways(numCluster);
+  for(int i=0; i < numCluster; i++)
+    pathways[i].resize(numCluster,0);
+  
+  bool ok=false;
+  cout << "cp.number_cluster " << cp.number_cluster << endl;
+  // check the pathways in the first lifeTimeLimit - 1 structures
+  int previous=timeSeries[0];
+  
+  for(int i=1; i< lifeTimeLimit-1; ++i){
+    ++pathways[timeSeries[i]][previous];
+    previous=timeSeries[i];
+  }
+  
+  
+  for(unsigned int i=lifeTimeLimit-1; i< timeSeries.size(); ++i){
+    //cout << lifetime[0] << " " << lifetime[1] << " " << lifetime[2] << " " << lifetime[3] << endl;
+     ++pathways[timeSeries[i]][previous];
+     previous = timeSeries[i];
+     
+    if(current==-1){
+      ok=true;
+	 
+      for(int j=1; j < lifeTimeLimit; ++j){
+	if(timeSeries[i]!=timeSeries[i-j]){
+	  ok=false;
+	  break;
+	}
+      }
+      if(ok) {
+	current=timeSeries[i];
+	lifetime[current]+=lifeTimeLimit;
+      }
+    }
+    else{
+      if(timeSeries[i-1] == timeSeries[i]){
+	++lifetime[current];
+      }
+      else{
+	// maybe it is just a break
+	ok=false;
+	for(int k=1; k<lifeTimeLimit; ++k){
+	  ok=true;
+	  for(int l=0; l <lifeTimeLimit; ++l){
+	    if((i+k+l)>=timeSeries.size() ||
+	       current != timeSeries[i+k+l]) {
+	      ok=false;
+	      break;
+	    }
+	  }
+	  if(ok==true){
+	    break;
+	  }
+	}
+	if(ok==false){
+	    
+	  ++occurence[current];
+	  if(lifeTimeLimit!=1)
+	    current=-1;
+	  else{
+	    current=timeSeries[i];
+	    ++lifetime[current];
+	  }
+	}
+      }
+    }
+  }
+    
+  if(current != -1)	  
+    ++occurence[current];
+
+  // write the output
+  cout << "#  cluster   visited  <lifetime>\n";
+  for(StructureSpecifier::const_iterator iter=cs.begin(), to=cs.end();
+      iter!=to; ++iter){
+    cout << setw(10) << *iter
+	 << setw(10) << occurence[*iter];
+    if(occurence[*iter])
+      cout << setw(12) << lifetime[*iter] * cp.dt / occurence[*iter];
+    else
+      cout << setw(12) << 0;
+    
+    cout << endl;
+  }
+  int sum=0;
+  for(unsigned int j=0; j<lifetime.size(); ++j) sum+=lifetime[j];
+  
+  cout << "#\n# Number of asocial structures " << cp.num - sum -1
+       << " (" << (cp.num - sum - 1) * cp.dt << ")\n"
+       << "# (i.e. not being part of any visit)\n#\n";
+
+  for(StructureSpecifier::const_iterator iter=cs.begin(), to=cs.end();
+      iter!=to; ++iter){
+    cout << "\n-- cluster " << *iter 
+	 <<" -------------------------------------------------------\n";
+    cout << "coming from: \n\t";
+    int count=0;
+    
+    for(int i=0; i< numCluster; i++){
+      if(i!=*iter && pathways[*iter][i] != 0){
+	if(count && count % 5 ==0) cout << "\n\t";
+	
+	ostringstream os;
+	os << i << " (" << pathways[*iter][i] << ")";
+	cout << setw(10) << os.str();
+	++count;
+      }
+    }
+    cout << "\ngoing to: \n\t";
+    count=0;
+    
+    for(int i=0; i< numCluster; i++){
+      if(i!=*iter && pathways[i][*iter] != 0){
+	if(count && count % 5 ==0) cout << "\n\t";
+	
+	ostringstream os;
+	os << i << " (" << pathways[i][*iter] << ")";
+	cout << setw(10) << os.str();
+	++count;
+      }
+    }
+    cout << "\n";
+    
+  }
+  
+}
+
+void colour_structures(cluster_parameter const & cp, 
+		       vector<int> const & timeSeries, 
+		       vector<int> const & rgb)
+{
+  
+      
+      int numCluster=cp.number_cluster;
+      if(cp.free) ++numCluster;
+      
+      if(rgb.back() != cp.num - 1)
+	throw gromos::Exception("postcluster", "rgb input is wrong\n"
+				"sum of entries is not equal to the total "
+				"number of structures\n");
+      vector< vector < int > > rgb_size(numCluster);
+      for(int i=0; i< numCluster; ++i)
+	rgb_size[i].resize(rgb.size(),0);
+      
+      // loop over the timeSeries
+      int colourCounter=0;
+      int tsSize=timeSeries.size();
+      
+      for(int i=0; i< tsSize; i++){
+	if(i >= rgb[colourCounter]) ++colourCounter;
+	++rgb_size[timeSeries[i]][colourCounter];
+      }
+      
+      ofstream fout("cluster_rgb.dat");
+      int start=0;
+      if(cp.free) start=1;
+      fout << "# cluster" << setw(rgb.size()*10-1) << "cumulative"
+	   << setw(rgb.size()*10+2) << "absolute";
+      fout << "\n#       ";
+      for(unsigned j=0; j < rgb.size(); ++j){
+	fout << setw(10) << rgb.size()-j;
+      }
+      fout << "  ";
+      
+      for(unsigned j=0; j < rgb.size(); ++j){
+	fout << setw(10) << j+1;
+      }
+      fout << endl;
+      
+      
+      for(int i=start; i<numCluster; ++i){
+	int sum=0;
+	for(unsigned j=0; j < rgb.size(); ++j){
+	  sum += rgb_size[i][j];
+	}
+	fout << setw(8) << i;
+	for(unsigned j=0; j < rgb.size(); ++j){
+	  fout << setw(10) << sum;
+	  sum -= rgb_size[i][rgb.size()-1-j];
+	}
+	fout << "  ";
+	for(unsigned j=0; j < rgb.size(); ++j){
+	  fout << setw(10) << rgb_size[i][j];
+	}
+	fout << endl;
+      }
+}
