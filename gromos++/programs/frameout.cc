d300ace2b30af21a38df93cf8c322dc90eb9b2c9
Old Commits <chris.oostenbrink@boku.ac.at>
2004-06-21 12:16:02 +0000

    added fitting possibility to frameout --chris

--- gromos++/programs/frameout.cc
+++ gromos++/programs/frameout.cc
@@ -1,6 +1,12 @@
 // frameout.cc
 
 #include <cassert>
+#include <vector>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <iostream>
 
 #include "../src/args/Arguments.h"
 #include "../src/args/BoundaryParser.h"
@@ -26,13 +32,8 @@
 #include "../src/gcore/Solvent.h"
 #include "../src/gcore/SolventTopology.h"
 #include "../src/gmath/Vec.h"
+#include "../src/utils/AtomSpecifier.h"
 
-#include <vector>
-#include <iomanip>
-#include <fstream>
-#include <sstream>
-#include <cstdlib>
-#include <iostream>
 
 using namespace std;
 using namespace gmath;
@@ -43,31 +44,31 @@ using namespace bound;
 using namespace args;
 using namespace fit;
 
+bool writeFrame(int i, std::string const & spec, vector<int> const & fnum);
+std::string fileName(int i, std::string const & ext);
+
+
 int main(int argc, char **argv){
 
-  char *knowns[] = {"topo", "traj", "pbc", "spec", "frames", "outformat", "include"};
-  int nknowns = 7;
+  char *knowns[] = {"topo", "traj", "pbc", "spec", "frames", "outformat", 
+		    "include", "ref", "atomsfit"};
+  int nknowns = 9;
 
   string usage = argv[0];
-  usage += "\n\t@topo <topology>\n";
-  usage += "\t@pbc <boundary type> <gather method>\n";
-  usage += "\t@spec <specification for writing out frames. either ALL, EVERY or SPEC>\n";
-  usage += "\t@frames <frames to be written out>\n";
-  usage += "\t@outformat <output format. either pdb, g96 or vmdam>\n"; 
-  usage += "\t@include <either SOLUTE (default), SOLVENT or ALL>\n";
-  usage += "\t@traj <trajectory files>\n";
+  usage += "\n\t@topo       <topology>\n";
+  usage += "\t@pbc        <boundary type> <gather method>\n";
+  usage += "\t[@spec      <specification for writing out frames. "
+    "either ALL (default), EVERY or SPEC>]\n";
+  usage += "\t[@frames    <frames to be written out>]\n";
+  usage += "\t[@outformat <output format. either pdb, g96 (default) or vmdam>]\n"; 
+  usage += "\t[@include   <either SOLUTE (default), SOLVENT or ALL>]\n";
+  usage += "\t[@ref       <reference structure to fit to>]\n";
+  usage += "\t[@atomsfit  <atoms to fit to>]\n";
+  usage += "\t@traj       <trajectory files>\n";
   
   try{
     Arguments args(argc, argv, nknowns, knowns, usage);
    
-    //smack in the framenumbers
-    vector<int> fnum;
-    for(Arguments::const_iterator it=args.lower_bound("frames");
-	  it != args.upper_bound("frames"); ++it){
-	int bla=atoi(it->second.c_str());
-        fnum.push_back(bla);
-    }      
-
     // read topology
     InTopology it(args["topo"]);
     System sys(it.system());
@@ -78,43 +79,86 @@ int main(int argc, char **argv){
     Boundary::MemPtr gathmethod = args::GatherParser::parse(args);
 
 
+    // do we want to fit to a reference structure
+    bool fit=false;
+    System refSys(sys);
+    Reference reffit(&refSys);
+    Vec cog(0.0,0.0,0.0);
+    
+    if(args.count("ref")>0){
+      fit=true;
+      refSys = sys;
+      // Parse boundary conditions
+      Boundary *pbc = BoundaryParser::boundary(refSys, args);
+      Boundary::MemPtr gathmethod = args::GatherParser::parse(args);
+   
+      // read reference coordinates...
+      InG96 ic(args["ref"]);
+      ic >> refSys;
+      (*pbc.*gathmethod)();
+ 
+      delete pbc;
+    
+      AtomSpecifier fitatoms(refSys);
+      
+      //try for fit atoms
+      if(args.count("atomsfit") > 0){
+	Arguments::const_iterator iter = args.lower_bound("atomsfit");
+	Arguments::const_iterator to = args.upper_bound("atomsfit");
+	for(;iter!=to;iter++) fitatoms.addSpecifier(iter->second);
+      }
+      else{
+	throw gromos::Exception("frameout", 
+				"If you want to fit (@ref) then give "
+				"atoms to fit to (@atomsfit)");
+      }
+      reffit.addAtomSpecifier(fitatoms);
+      cog=PositionUtils::cog(refSys, reffit);
+    }
+    // does this work if nothing is set?
+    RotationalFit rf(&reffit);
+      
     // parse includes
     string inc = "SOLUTE";
-    try{
-      string format = args["include"];
-      if(format == "ALL")
-	inc = "ALL";
-      else if(format == "SOLUTE")
-	inc = "SOLUTE";
-      else if (format == "SOLVENT")
-	inc = "SOLVENT";
-      else 
-	throw gromos::Exception("frameout","include format "+format+" unknown.\n");
+    if(args.count("include")>0){
+      inc = args["include"];
+      if(inc != "SOLUTE" && inc !="ALL" && inc!="SOLVENT")
+	throw gromos::Exception("frameout",
+				"include format "+inc+" unknown.\n");
     }
-    catch(Arguments::Exception &e){
-    }  
+    
     // parse spec
-
     string spec = "ALL";
-    try{
-      string format = args["spec"];
-      if(format == "ALL")
-	spec = "ALL";
-      else if(format == "EVERY")
-	spec = "EVERY";
-      else if (format == "SPEC")
-	spec = "SPEC";
-      else 
-	throw gromos::Exception("frameout","spec format "+format+" unknown.\n");
+    vector<int> fnum;
+    if(args.count("spec")>0){
+      spec = args["spec"];
+      if(spec!="ALL" && spec !="EVERY" && spec !="SPEC")
+	throw gromos::Exception("frameout",
+				"spec format "+spec+" unknown.\n");
+      if(spec=="EVERY" || spec=="SPEC"){
+	//smack in the framenumbers
+	for(Arguments::const_iterator it=args.lower_bound("frames");
+	    it != args.upper_bound("frames"); ++it){
+	  int bla=atoi(it->second.c_str());
+	  fnum.push_back(bla);
+	}      
+	if(fnum.size()==0){
+	  throw gromos::Exception("frameout", 
+				  "if you give EVERY or SPEC you have to use "
+				  "@frames as well");
+	}
+	if(fnum.size()!=1 && spec=="EVERY"){
+	  throw gromos::Exception("frameout",
+				  "if you gice EVERY you have to give exactly"
+				  " one number with @frames");
+	}
+      }
     }
-    catch(Arguments::Exception &e){
-      //  pbc = new Vacuum(&sys);
-    }  
-
-    // outformat
+    
+    // parse outformat
     OutCoordinates *oc;
     string ext = ".g96";
-    try{
+    if(args.count("outformat")>0){
       string format = args["outformat"];
       if(format == "pdb"){
 	oc = new OutPdb();
@@ -128,102 +172,42 @@ int main(int argc, char **argv){
       else 
         throw gromos::Exception("frameout","output format "+format+" unknown.\n");
     }
-    catch(Arguments::Exception &){
+    else{
       oc = new OutG96S();
     }
-
     
     // loop over all trajectories
     InG96 ic;
     int numFrames = 0;
-    int every = 0;
     for(Arguments::const_iterator iter=args.lower_bound("traj");
 	iter!=args.upper_bound("traj"); ++iter){
       ic.open(iter->second);  
       // loop over all frames
       while(!ic.eof()){
-	numFrames++;every++;
+	numFrames++;
 	ic.select(inc);
 	ic >> sys;
-	
-	(*pbc.*gathmethod)();
-	
-	if (spec == "ALL"){	
-	  char outFile[]="FRAME";
-	  ostringstream out;
-	  if (numFrames < 10){
-	    out <<outFile<<"_"<<"0000"<<numFrames<<ext;
+
+	if(writeFrame(numFrames, spec, fnum)){
+	  
+	  (*pbc.*gathmethod)();
+	 
+	  if(fit){
+	    rf.fit(&sys);
+	    PositionUtils::translate(&sys, cog);	  
 	  }
-	  else if (numFrames < 100){
-	    out <<outFile<<"_"<<"000"<<numFrames<<ext;
-	  }
-	  else if (numFrames < 1000){
-	    out <<outFile<<"_"<<"00"<<numFrames<<ext;
-	  }
-	  else {
-	    out <<outFile<<"_"<<"0"<<numFrames<<ext;
-	  }
-	  ofstream os(out.str().c_str()); 
+	  
+	  string file=fileName(numFrames, ext);
+	  
+	  ofstream os(file.c_str());
+	  
 	  oc->open (os);       
 	  oc->select(inc);
-	  oc->writeTitle(out.str());
+	  oc->writeTitle(file);
 	  
 	  *oc << sys;
 	  os.close();
 	}
-	else if (spec == "EVERY" && (every-fnum[0] == 0)){
-	  char outFile[]="FRAME";
-	  ostringstream out;
-	  if (numFrames < 10){
-	    out <<outFile<<"_"<<"0000"<<numFrames<<ext;
-	  }
-	  else if (numFrames < 100){
-	    out <<outFile<<"_"<<"000"<<numFrames<<ext;
-	  }
-	  else if (numFrames < 1000){
-	    out <<outFile<<"_"<<"00"<<numFrames<<ext;
-	  }
-	  else {
-	    out <<outFile<<"_"<<"0"<<numFrames<<ext;
-	  } 
-	  ofstream os(out.str().c_str()); 
-	  oc->open (os);       
-	  oc-> select(inc);
-	  oc->writeTitle(out.str());
-	  
-	  *oc << sys;
-	  os.close();	       
-	  every=0;
-	}
-	
-	else if (spec == "SPEC"){
-	  for (int i=0; i< int (fnum.size()); ++i){
-	    if (numFrames == fnum[i]){
-	      char outFile[]="FRAME";
-	      ostringstream out;
-	      if (numFrames < 10){
-		out <<outFile<<"_"<<"0000"<<numFrames<<ext;
-	      }
-	      else if (numFrames < 100){
-		out <<outFile<<"_"<<"000"<<numFrames<<ext;
-	      }
-	      else if (numFrames < 1000){
-		out <<outFile<<"_"<<"00"<<numFrames<<ext;
-	      }
-	      else {
-                out <<outFile<<"_"<<"0"<<numFrames<<ext;
-	      } 
-	      ofstream os(out.str().c_str()); 
-	      oc->open (os);       
-	      oc-> select(inc);
-	      oc->writeTitle(out.str());
-	      
-	      *oc << sys;
-	       os.close();
-	    }
-	  }
-	}
-    	
       }
       ic.close();
     }
@@ -233,3 +217,35 @@ int main(int argc, char **argv){
     exit(1);
   }
   return 0;
+}
+
+bool writeFrame(int i, std::string const & spec, vector<int> const & fnum)
+{
+  else if(spec=="SPEC"){
+    for(unsigned int j=0; j< fnum.size(); ++j){
+      if(fnum[j]==i) return true;
+    }
+  }
+  return false;
+}
+
+std::string fileName(int numFrames, std::string const & ext)
+{
+  ostringstream out;
+  string outFile="FRAME";
+  if (numFrames < 10){
+    out <<outFile<<"_"<<"0000"<<numFrames<<ext;
+  }
+  else if (numFrames < 100){
+    out <<outFile<<"_"<<"000"<<numFrames<<ext;
+  }
+  else if (numFrames < 1000){
+    out <<outFile<<"_"<<"00"<<numFrames<<ext;
+  }
+  else {
+    out <<outFile<<"_"<<"0"<<numFrames<<ext;
+  } 
+
+  return out.str();
+}
+
