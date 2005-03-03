f26705edc54e5ae4a6e41b95343d7eddb1c1398b
Markus Christen <m.christen@fake.com>
2005-03-03 17:03:45 +0000

    gromos96 gather bug

--- gromos++/src/bound/TruncOct.cc
+++ gromos++/src/bound/TruncOct.cc
@@ -9,6 +9,8 @@
 #include "../gcore/Box.h"
 #include <cmath>
 
+#include <iostream> 
+
 using bound::TruncOct;
 using gmath::Vec;
 using namespace gcore;
@@ -58,71 +60,74 @@ void TruncOct::gathergr(){
 
 
 void TruncOct::gather(){
-
+  
   if (!sys().hasBox) throw gromos::Exception("Gather problem",  
-                              "System does not contain Box block! Abort!");
-
-  if (sys().box()[0] == 0 || sys().box()[1] == 0 || sys().box()[2] == 0) throw gromos::Exception("Gather problem",  
-                              "Box block contains element(s) of value 0.0! Abort!");  
+					     "System does not contain Box block! Abort!");
+  
+  if (sys().box()[0] == 0 || sys().box()[1] == 0 || sys().box()[2] == 0)
+    throw gromos::Exception("Gather problem",  
+			    "Box block contains element(s) of value 0.0! Abort!");  
 
   for(int i=0; i<sys().numMolecules();++i){
     Molecule &mol=sys().mol(i);
-    mol.pos(0)=nim(reference(i),mol.pos(0),sys().box());
+
+    mol.pos(0)=nim(reference(0),mol.pos(0),sys().box());
     for(int j=1;j<mol.numPos();++j)
       mol.pos(j)=nim(mol.pos(j-1),mol.pos(j),sys().box());
   }
- // do the solvent 
-   Solvent &sol=sys().sol(0);
-   for(int i=0;i<sol.numPos();i+= sol.topology().numAtoms()){
-   sol.pos(i)=nim(reference(0),sol.pos(i),sys().box());
-   for (int j=i+1;j < (i+sol.topology().numAtoms());++j){
-    sol.pos(j)=nim(sol.pos(j-1),sol.pos(j),sys().box());
-   }
+  // do the solvent 
+  Solvent &sol=sys().sol(0);
+  for(int i=0;i<sol.numPos();i+= sol.topology().numAtoms()){
+    sol.pos(i)=nim(reference(0),sol.pos(i),sys().box());
+    for (int j=i+1;j < (i+sol.topology().numAtoms());++j){
+      sol.pos(j)=nim(sol.pos(j-1),sol.pos(j),sys().box());
+    }
   }
 }
 
 void TruncOct::coggather(){
 
-   if (!sys().hasBox) throw gromos::Exception("Gather problem",  
-                              "System does not contain Box block! Abort!");
-
-   if (sys().box()[0] == 0 || sys().box()[1] == 0 || sys().box()[2] == 0) throw gromos::Exception("Gather problem",  
-                              "Box block contains element(s) of value 0.0! Abort!");  
-
-   Molecule &mol=sys().mol(0);
-   Solvent &sol=sys().sol(0);
+  if (!sys().hasBox) throw gromos::Exception("Gather problem",  
+					     "System does not contain Box block! Abort!");
   
-   Vec ref(0.0,0.0,0.0);
-   Vec cog;
-   int atoms=0;
-
-   // do mol(0) with respect to ref (0,0,0)
-   mol.pos(0)=nim(ref,mol.pos(0),sys().box());
-   for(int j=1;j<mol.numAtoms();++j){
-     mol.pos(j)=nim(mol.pos(j-1),mol.pos(j),sys().box());}
-
-   // calculate COG of mol(0)
-    for (int i=0;i < mol.numAtoms(); i++) {
-      cog = cog + mol.pos(i);
-      ++atoms;
+  if (sys().box()[0] == 0 || sys().box()[1] == 0 || sys().box()[2] == 0)
+    throw gromos::Exception("Gather problem",  
+			    "Box block contains element(s) of value 0.0! Abort!");  
+  
+  Molecule &mol=sys().mol(0);
+  Solvent &sol=sys().sol(0);
+  
+  Vec ref(0.0,0.0,0.0);
+  Vec cog;
+  int atoms=0;
+  
+  // do mol(0) with respect to ref (0,0,0)
+  mol.pos(0)=nim(ref,mol.pos(0),sys().box());
+  for(int j=1;j<mol.numAtoms();++j){
+    mol.pos(j)=nim(mol.pos(j-1),mol.pos(j),sys().box());}
+  
+  // calculate COG of mol(0)
+  for (int i=0;i < mol.numAtoms(); i++) {
+    cog = cog + mol.pos(i);
+    ++atoms;
+  }
+  cog = (1.0/double(atoms))*cog;
+  
+  // do the rest of the molecules
+  for(int i=1;i<sys().numMolecules();++i){
+    Molecule &mol=sys().mol(i);
+    mol.pos(0)=nim(cog,mol.pos(0),sys().box());      
+    for(int j=1;j<mol.numPos();++j){
+      mol.pos(j)=nim(mol.pos(j-1),mol.pos(j),sys().box());
     }
-   cog = (1.0/double(atoms))*cog;
-
-   // do the rest of the molecules
- for(int i=1;i<sys().numMolecules();++i){
-      Molecule &mol=sys().mol(i);
-      mol.pos(0)=nim(cog,mol.pos(0),sys().box());      
-   for(int j=1;j<mol.numPos();++j){
-    mol.pos(j)=nim(mol.pos(j-1),mol.pos(j),sys().box());
-   }
- }
-
- // do the solvent 
-   for(int i=0;i<sol.numPos();i+= sol.topology().numAtoms()){
-   sol.pos(i)=nim(cog,sol.pos(i),sys().box());
-   for (int j=i+1;j < (i+sol.topology().numAtoms());++j){
-    sol.pos(j)=nim(sol.pos(j-1),sol.pos(j),sys().box());
-   }
+  }
+  
+  // do the solvent 
+  for(int i=0;i<sol.numPos();i+= sol.topology().numAtoms()){
+    sol.pos(i)=nim(cog,sol.pos(i),sys().box());
+    for (int j=i+1;j < (i+sol.topology().numAtoms());++j){
+      sol.pos(j)=nim(sol.pos(j-1),sol.pos(j),sys().box());
+    }
   }
 } 
 
