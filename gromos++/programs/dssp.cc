16b694d45fe6a32a577de8b1033196140bf69c91
Daniel Trzesniak <d.trzeniak@fake.com>
2005-03-03 12:09:41 +0000

    added atom specifier

--- gromos++/programs/dssp.cc
+++ gromos++/programs/dssp.cc
@@ -42,40 +42,53 @@ using namespace args;
 using namespace gmath;
 using namespace utils;
 using namespace std;
+using utils::AtomSpecifier;
 
 int main(int argc, char **argv){
-
-  char *knowns[] = {"topo", "pbc", "time", "nthframe", "traj"};
-  int nknowns = 5;
-
+  
+  char *knowns[] = {"topo", "pbc", "protein", "time", "nthframe", "traj"};
+  int nknowns = 6;
+  
   string usage = argv[0];
   usage += "\n\t@topo     <topology>\n";
   usage += "\t@pbc      <boundary type>\n";
+  usage += "\t@protein  <atomspecifier for the protein>\n";
   usage += "\t@time     <time and dt>\n";
   usage += "\t@nthframe <write every nth frame> (optional, default is 1)\n";
   usage += "\t@traj     <trajectory files>\n";
   
- 
+  
   try{
     Arguments args(argc, argv, nknowns, knowns, usage);
-   
-
+    
     args.check("topo",1);
     InTopology it(args["topo"]);
     System sys(it.system());
-
+    
     int nthFrame = 1;
     try{
       args.check("nthframe", 1);
       nthFrame = atoi(args["nthframe"].c_str());
     }
     catch (const gromos::Exception &e){}
-
-
+    
+    // get the protein atoms
+    AtomSpecifier prot(sys);
+    {
+      Arguments::const_iterator iter=args.lower_bound("protein");
+      Arguments::const_iterator to=args.upper_bound("protein");
+      for(;iter!=to;iter++) {
+	prot.addSpecifier(iter->second.c_str());
+	cout << iter->second.c_str() << " ";
+      }
+    }
+    cout << "\n# In the output the residues are numbered sequentially"
+	 << " from 1 to n and not according to @protein!\n";
+    
     Dssp SecStr(sys,args);
-
+    
     //get time
-
+    
     double time=0, dt=1; 
     {
       Arguments::const_iterator iter=args.lower_bound("time");
@@ -86,15 +99,16 @@ int main(int argc, char **argv){
       if(iter!=args.upper_bound("time"))
         dt=atof(iter->second.c_str());
     }
-
+    
     SecStr.settime(time, dt);
-    SecStr.determineAtoms();
-    SecStr.calcHintra_init(); 
-
+    SecStr.calcnumres(prot);
+    SecStr.determineAtoms(prot);
+    SecStr.calcHintra_init(prot); 
+    
     InG96 ic;
-  
+    
     int skipFrame = 0;
-
+    
     for(Arguments::const_iterator 
 	  iter=args.lower_bound("traj"),
 	  to=args.upper_bound("traj");
@@ -117,11 +131,15 @@ int main(int argc, char **argv){
       ic.close();
     }
     SecStr.writeSummary(cout);
-  
+    
   }
-  catch (const gromos::Exception &e){
+  catch (const gromos::Exception &e) {
     cerr << e.what() << endl;
     exit(1);
   }
   return 0;
 }
+
+
+
+
