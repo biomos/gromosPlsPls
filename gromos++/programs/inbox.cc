560b179dff1d3d7612408a2b73c56a4ffc53397b
Old Commits <chris.oostenbrink@boku.ac.at>
2006-08-25 00:59:41 +0000

    doxygen information --chris

--- gromos++/programs/inbox.cc
+++ gromos++/programs/inbox.cc
@@ -1,9 +1,55 @@
-/* inbox.cc
- * Alice Glaettli, Markus Christen, Chris Oostenbrink
- * dedicated to Indira Chandrasekhar
-*/
+/**
+ * @file inbox.cc
+ * Force atoms in the simulation box
+ */
+
+/**
+ * @page programs Program Documentation
+ *
+ * @anchor inbox
+ * @section inbox Force atoms in the simulation box
+ * @author @ref AG
+ * @date 24.8.2006
+ *
+ * Even though all GROMOS programs correct for periodic boundary conditions
+ * whenever necessary, it can sometimes be quite cumbersome to display a
+ * simulation box that contains all molecules at clear distance from one
+ * another. For simulations containing one or a few solute molecules, program
+ * frameout in combination with the proper gather method will be sufficient,
+ * but especially in molecular systems consisting of many solute molecules, it
+ * may happen that no settings exactly work.
+ *
+ * Program inbox, forces all solute and solvent atoms to be inside a single
+ * computational box, regardless of their connectivity and relative distances
+ * to other atoms. Before applying the box boundaries, the molecular system may
+ * be shifted by a user specified vector. The box will be roughly centered
+ * around the centre of geometry of all solute atoms.
+ *
+ * <b>arguments:</b>
+ * <table border=0 cellpadding=0>
+ * <tr><td> \@topo</td><td>&lt;topology&gt; </td></tr>
+ * <tr><td> \@pbc</td><td>&lt;boundary type&gt; </td></tr>
+ * <tr><td> \@traj</td><td>&lt;trajectory files&gt; </td></tr>
+ * <tr><td> [\@shift</td><td>&lt;vector to shift&gt;] </td></tr>
+ * </table>
+ *
+ *
+ * Example:
+ * @verbatim
+  inbox
+    @topo   ex.top
+    @pbc    r
+    @traj   exref.coo
+    @shift  0.6 -0.5 0.4
+ @endverbatim
+ *
+ * <hr>
+ */
 
 #include <cassert>
+#include <vector>
+#include <iomanip>
+#include <iostream>
 
 #include "../src/args/Arguments.h"
 #include "../src/args/BoundaryParser.h"
@@ -21,9 +67,6 @@
 #include "../src/bound/Boundary.h"
 #include "../src/bound/TruncOct.h"
 #include "../src/gmath/Vec.h"
-#include <vector>
-#include <iomanip>
-#include <iostream>
 
 using namespace gcore;
 using namespace gio;
@@ -37,11 +80,11 @@ int main(int argc, char **argv){
   char *knowns[] = {"topo", "pbc", "traj", "shift"};
   int nknowns = 4;
 
-  string usage = argv[0];
-  usage += "\n\t@topo <topology>\n";
-  usage += "\t@pbc <boundary type>\n";
-  usage += "\t@traj <trajectory files>\n";
-  usage += "\t@shift <vector to shift>\n";
+  string usage = "# " + string(argv[0]);
+  usage += "\n\t@topo    <topology>\n";
+  usage += "\t@pbc     <boundary type>\n";
+  usage += "\t@traj    <trajectory files>\n";
+  usage += "\t[@shift  <vector to shift>]\n";
   
  
   try{
@@ -67,15 +110,11 @@ int main(int argc, char **argv){
     if(iter!=to)       
       shift[2]=atof(iter->second.c_str());
 
-     
-    
     // parse boundary conditions
     Boundary *pbc;
     pbc = BoundaryParser::boundary(sys, args);
     //Boundary::MemPtr gathmethod = args::GatherParser::parse(args);
 
-
-    
     // define input coordinate
     InG96 ic;
     ic.open(args["traj"].c_str());
@@ -84,7 +123,6 @@ int main(int argc, char **argv){
     // output
     OutCoordinates *oc;
     oc = new OutPdb();
-
     
     oc->open(cout);
     oc->select("ALL");  
@@ -109,7 +147,9 @@ int main(int argc, char **argv){
 	
 	cog=pbc->nearestImage(origin,cog, sys.box());
 	for(int j=0; j<sys.mol(i).numAtoms(); j++){
-	  sys.mol(i).pos(j)=pbc->nearestImage(cog, sys.mol(i).pos(j), sys.box());
+	  sys.mol(i).pos(j)=pbc->nearestImage(cog, 
+					      sys.mol(i).pos(j), 
+					      sys.box());
 	}
 	 
 	  
@@ -119,12 +159,16 @@ int main(int argc, char **argv){
 	  i+=sys.sol(0).topology().numAtoms()){
 	sys.sol(0).pos(i)+=shift;
 	
-        sys.sol(0).pos(i)=pbc->nearestImage(origin, sys.sol(0).pos(i), sys.box());
+        sys.sol(0).pos(i)=pbc->nearestImage(origin, 
+					    sys.sol(0).pos(i), 
+					    sys.box());
 	
 	for(int j=1; j<sys.sol(0).topology().numAtoms(); j++){
 	  sys.sol(0).pos(i+j)+=shift;
 	  
-	  sys.sol(0).pos(i+j)=pbc->nearestImage(sys.sol(0).pos(i), sys.sol(0).pos(i+j), sys.box());
+	  sys.sol(0).pos(i+j)=pbc->nearestImage(sys.sol(0).pos(i), 
+						sys.sol(0).pos(i+j), 
+						sys.box());
 	}
       }
       
