b1f53f8b23a0eafac3cbd2797e8b88c262e825f7
Chris Oostenbrink <chris.oostenbrink@boku.ac.at>
2007-07-23 13:59:44 +0000

    Added AtomDistances for a difference measurement --chris

---
+++ gromos++/src/fit/AtomDistances.cc
@@ -0,0 +1,42 @@
+// fit_AtomDistances.cc
+//includes explicit calls to gsl now
+
+#include <cassert>
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+#include "../gcore/System.h"
+#include "../gcore/Molecule.h"
+#include "../gmath/Matrix.h"
+#include "../gmath/Vec.h"
+#include "PositionUtils.h"
+#include "../utils/AtomSpecifier.h"
+
+#include "AtomDistances.h"
+
+using gmath::Matrix;
+using gmath::Vec;
+using namespace fit;
+using namespace std;
+
+
+
+double AtomDistances::dist(vector<Vec> const &ref, 
+			       vector<Vec> const &sys)const
+{
+  double dist2=0.0;
+  for(size_t i=0; i< ref.size(); ++i){
+    for(size_t j=i+1; j < ref.size(); ++j){
+      for(int b=0;b<3;++b){
+	dist2 += 
+	  ((ref[i][b] - ref[j][b]) - (sys[i][b] - sys[j][b])) *
+	  ((ref[i][b] - ref[j][b]) - (sys[i][b] - sys[j][b]));
+      }
+    }
+  }
+  int N = ref.size() * (ref.size() - 1) / 2;
+  
+  return sqrt(dist2/N);
+}
---
+++ gromos++/src/fit/AtomDistances.h
@@ -0,0 +1,70 @@
+// fit_AtomDistances.h
+
+#ifndef INCLUDED_FIT_ATOMDISTANCES
+#define INCLUDED_FIT_ATOMDISTANCES
+
+
+#ifndef INCLUDED_GROMOS_EXCEPTION
+#include "../gromos/Exception.h"
+#endif
+
+namespace fit{
+  /**
+   * Class AtomDistances
+   * This class calculates the structural difference between to structures
+   * based on the interatomic distances
+   *
+   * @class AtomDistances
+   * @author Chris Oostenbrink
+   * @ingroup fit
+   * @sa fit::FastRotationalFit
+   * @sa fit::TranslationalFit
+   * @sa utils::Rmsd
+   */
+  class AtomDistances{
+    /**
+     * atoms to calculate the distances for
+     */
+    std::vector<bool> d_dist_spec;
+    /**
+     * number of atoms used in dist calculation
+     */
+    int d_dist_num_atoms;
+
+  public:
+    /**
+     * Constructor: no specifications
+     */
+    AtomDistances() 
+      : d_dist_spec(0), d_dist_num_atoms(0) {};
+    /**
+     * Constructor
+     */
+    AtomDistances(std::vector<bool> dist_spec)
+      : d_dist_spec(dist_spec),
+	d_dist_num_atoms(0){
+      for(size_t i=0; i<d_dist_spec.size(); ++i)
+	if(d_dist_spec[i]) ++d_dist_num_atoms;
+    };
+    
+    /**
+     * calculate the distance (using atoms specified for dist) on (reduced) atom positions
+     * using the specified rotation matrix on sys.
+     */
+    double dist(std::vector<gmath::Vec> const &ref,
+		std::vector<gmath::Vec> const &sys)const;
+    
+
+    /**
+     * FastRotationalFit exception
+     */
+    struct Exception: public gromos::Exception{
+      Exception(const std::string &what): 
+	gromos::Exception("RotationalFit",what){}
+    };
+    
+  };
+
+}
+
+#endif    
