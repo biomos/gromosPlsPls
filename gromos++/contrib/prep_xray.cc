9ee579b7d25263ab2d3bcb50c7010c7332cb0e97
Nathan Schmid <an.schmid@fake.com>
2009-04-08 15:16:53 +0000

    new fancy prep_xray with resolution determination and filtering, and docu

--- gromos++/contrib/prep_xray.cc
+++ gromos++/contrib/prep_xray.cc
@@ -17,6 +17,8 @@
  * atoms according to their integer atom codes.
  * The spacegroup, cell, resolution and standard B-factor of the atoms are written
  * out the outfile in the correct format.
+ * The reflection list can be filtered according to the given resolution range.
+ * If no resolution is given, it is determined automatically.
  *
  * <b>arguments:</b>
  * <table border=0 cellpadding=0>
@@ -25,7 +27,7 @@
  * <tr><td> \@map</td><td>&lt;file with IAC-to-elementname mapping&gt; </td></tr>
  * <tr><td> \@spacegroup</td><td>&lt;spacegroup in Hermann-Maauguin format&gt; </td></tr>
  * <tr><td> \@cell</td><td>&lt;cell in form: a b c alpha beta gamma&gt; </td></tr>
- * <tr><td> \@resolution</td><td>&lt;scattering resolution&gt; </td></tr>
+ * <tr><td> \@resolution</td><td>&lt;scattering resolution, from and to&gt; </td></tr>
  * <tr><td> \@bfactor</td><td>&lt;standard B-factor (in @f$\mathrm{nm}^2@f$)&gt;</td></tr>
  * </table>
  *
@@ -40,7 +42,7 @@
     @map           ex.map
     @spacegroup    P 21 21 21
     @cell          5.00 5.10 5.20 90.0 90.0 90.0
-    @resolution    0.15
+    @resolution    0.3 0.15
     @bfactor       0.01
  @endverbatim
  *
@@ -48,6 +50,7 @@
  */
 
 #include <cassert>
+#include <streambuf>
 #include <vector>
 #include <map>
 #include <iomanip>
@@ -55,6 +58,7 @@
 #include <cmath>
 #include <sstream>
 #include <fstream>
+#include <limits>
 #include <ios>
 
 #include "../src/args/Arguments.h"
@@ -71,6 +75,11 @@ using namespace gcore;
 using namespace args;
 using namespace gio;
 using namespace std;
+
+// Additional Clipper Headers
+#include "../config.h"
+#ifdef HAVE_CLIPPER
+#include <clipper/clipper.h>
 
 // Struct for cif-data
 
@@ -190,7 +199,7 @@ int main(int argc, char *argv[]) {
   usage += "\t@map            <gromos atom code to element mapping file>\n";
   usage += "\t@spacegroup     <spacegroup in hermann-mauguin form>\n";
   usage += "\t@cell           <cell in form: a b c alpha beta gamma>\n";
-  usage += "\t@resolution     <scattering resolution>\n";
+  usage += "\t@resolution     <scattering resolution min max>\n";
   usage += "\t@bfactor        <standard B-factor>\n";
 
   // known arguments...
@@ -244,17 +253,47 @@ int main(int argc, char *argv[]) {
                 "Not enough cell parameters");
       }
     }
+    // create the clipper cell
+    clipper::Cell clipperCell(
+            clipper::Cell_descr(cell[0]*10.0f, cell[1]*10.0f, cell[2]*10.0f,
+                cell[3], cell[4], cell[5]));
 
     //read in scattering resolution
-    float reso;
+    bool have_reso = false;
+    double reso_min = 0.0, reso_max = numeric_limits<double>::max();
     {
-      std::istringstream is(args["resolution"]);
-      if (!(is >> reso)) {
+      if (args.count("resolution") == 2) {
+        have_reso = true;
+        Arguments::const_iterator iter = args.lower_bound("resolution");
+        std::istringstream is(iter->second);
+        if (!(is >> reso_min)) {
+          throw gromos::Exception(argv[0],
+                  "Resolution minimum not numeric");
+        }
+        ++iter;
+
+        is.clear();
+        is.str(iter->second);
+        if (!(is >> reso_max)) {
+          throw gromos::Exception(argv[0],
+                  "Resolution maximum not numeric");
+        }
+      } // two resolutions
+      else if (args.count("resolution") == -1) {
+        have_reso = false;
+      } else {
         throw gromos::Exception(argv[0],
-                "Resolution parameter not numeric");
+                  "Please give a resolution range or let the program determine it");
       }
     }
 
+    // swap them if someone is confused by the fact that the low numeric
+    // value is actually a high resolution
+    if (have_reso && reso_min <= reso_max) {
+      const double tmp = reso_min;
+      reso_min = reso_max;
+      reso_max = tmp;
+    }
     //read in standard B-factor
     float bfactor;
     {
@@ -294,7 +333,20 @@ int main(int argc, char *argv[]) {
     cout << "XRAYRESSPEC\n";
     cout << "#" << setw(4) << "H" << setw(5) << "K" << setw(5) << "L";
     cout << setw(10) << "SF" << setw(12) << "STDDEV_SF\n";
+
+    int filtered = 0;
     for (unsigned int i = 0; i < cifdata.size(); ++i) {
+      clipper::HKL hkl(cifdata[i].h, cifdata[i].k, cifdata[i].l);
+      double hkl_resolution = sqrt(1.0 / hkl.invresolsq(clipperCell)) / 10.0;
+      if (have_reso) {
+        if (hkl_resolution < reso_max || hkl_resolution > reso_min) {
+          ++filtered;
+          continue;
+        }
+      } else {
+        // determine maximal resolution (minimal numerical number)
+        reso_max = std::min(reso_max, hkl_resolution);
+      }
       cout << setw(5) << cifdata[i].h;
       cout << setw(5) << cifdata[i].k;
       cout << setw(5) << cifdata[i].l;
@@ -302,6 +354,9 @@ int main(int argc, char *argv[]) {
       cout << setw(11) << cifdata[i].stddev_sf << "\n";
     }
     cout << "END\n";
+
+    if (filtered)
+      cerr << filtered << " reflections have been filtered away." << endl;
 
     //XRAYELEMENTSPEC
     cout << "XRAYELEMENTSPEC\n";
@@ -333,7 +388,7 @@ int main(int argc, char *argv[]) {
     cout << "# " << setw(7) << "CELL" << setw(60) << "RESO" << setw(12) << "BFACTOR\n";
     cout << setw(10) << cell[0] << setw(10) << cell[1] << setw(10) << cell[2];
     cout << setw(10) << cell[3] << setw(10) << cell[4] << setw(10) << cell[5];
-    cout << setw(10) << reso << setw(10) << bfactor << "\n";
+    cout << setw(10) << reso_max << setw(10) << bfactor << "\n";
     cout << "END\n";
 
   } catch (const gromos::Exception &e) {
@@ -342,3 +397,13 @@ int main(int argc, char *argv[]) {
   }
   return 0;
 }
+
+#else
+int main(int argc, char **argv) {
+  cerr << "You have to compile GROMOS++ with CCP4"
+              " and clipper libraries in order to use this program.\n"
+              "Use --with-ccp4 and --with-clipper for configuration."
+          << endl;
+  return 1;
+}
+#endif
