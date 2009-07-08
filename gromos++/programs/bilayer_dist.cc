964c578df69e0f20cce5e2cbf0d15f1976c7c603
Bruno Horta <bruno.horta@gmail.com>
2009-07-08 13:35:52 +0000

    Program added: bilayer_dist

---
+++ gromos++/programs/bilayer_dist.cc
@@ -0,0 +1,251 @@
+/**
+ * @file bilayer_dist.cc
+ * Calculates probability distribution along bilayer normal
+ */
+
+/**
+ * @page programs Program Documentation
+ *
+ * @anchor bilayer_dist
+ * @section bilayer_dist Calculates probability distribution along bilayer normal
+ * @author @ref bh
+ * @date 8-7-09
+ *
+ * Distributions along the bilayer normal are useful to characterise membrane
+ * systems. This program calculates the distribution of a given set of atoms
+ * with respect to the center of mass of another given set of atoms (usually,
+ * this set will include all bilayer atoms to give a distribution of atoms with
+ * respect to the bilayer center of mass).
+ *
+ * By default, all distributions are normalised to unity. However, the user
+ * may be interested in density distributions. In this case, the flag
+ * \@density must be included.
+ *
+ * The user can also obtain mass or electron densities with the help of the
+ * \@mult argument. This will multiply the distribution by a given number. If the
+ * user wants to calculate electron density profiles the \@mult and \@density
+ * arguments must be given.
+ *
+ * In some cases, the bilayer is not centered in the periodic box and the center
+ * of mass will not be in between the two layers but in the bulk of the solvent
+ * instead. The argument \@translate can be used to circumvent this problem.
+ *
+ * <b>arguments:</b>
+ * <table border=0 cellpadding=0>
+ * <tr><td> \@topo</td><td>&lt;molecular topology file&gt; </td></tr>
+ * <tr><td> \@pbc</td><td>&lt;boundary type&gt; </td></tr>
+ * <tr><td> [\@time</td><td>&lt;@ref utils::Time "time and dt"&gt;] </td></tr>
+ * <tr><td> \@atoms</td><td>&lt;@ref AtomSpecifier&gt; </td></tr>
+ * <tr><td> \@selection</td><td>&lt;@ref AtomSpecifier&gt; </td></tr>
+ * <tr><td> [\@translate</td><td>&lt;optional to translate box&gt;] </td></tr>
+ * <tr><td> [\@grid</td><td>&lt;integer; default: 100&gt;] </td></tr>
+ * <tr><td> \@traj</td><td>&lt;trajectory files&gt; </td></tr>
+ * <tr><td> [\@mult</td><td>&lt;double; default: 1&gt;] </td></tr>
+ * <tr><td> [\@density</td><td>&lt;optional to calculate density&gt;] </td></tr>
+ * </table>
+ *
+ *
+ * Example:
+ * @verbatim
+  bilayer_dist
+    @topo             ex.top
+    @pbc              r
+    [@time            0 1]
+    @atoms            1-72:a
+    @selection        1-72:P
+    @translate
+    @density
+    @mult             15
+    @traj             ex.tr
+ @endverbatim
+ *
+ * <hr>
+ */
+
+
+#include <cassert>
+#include <cmath>
+#include <map>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+
+#include "../src/args/Arguments.h"
+#include "../src/args/BoundaryParser.h"
+#include "../src/args/GatherParser.h"
+#include "../src/gio/InG96.h"
+#include "../src/gio/Ginstream.h"
+#include "../src/gcore/System.h"
+#include "../src/gio/InTopology.h"
+#include "../src/bound/Boundary.h"
+#include "../src/gmath/Vec.h"
+#include "../src/utils/AtomSpecifier.h"
+#include "../src/utils/PropertyContainer.h"
+#include "../src/utils/Property.h"
+#include "../src/utils/Time.h"
+#include "../src/gcore/Box.h"
+
+using namespace gcore;
+using namespace gio;
+using namespace bound;
+using namespace args;
+using namespace gmath;
+using namespace utils;
+using namespace std;
+
+int main(int argc, char** argv) {
+  Argument_List knowns;
+  knowns << "topo" << "pbc" << "time" << "atoms" << "selection" << "translate"
+          << "grid" << "traj" << "mult" << "density";
+
+  string usage = "# " + string(argv[0]);
+  usage += "\n\t@topo           <molecular topology file>\n";
+  usage += "\t@pbc            <boundary type> [<gathermethod>]\n";
+  usage += "\t[@time           <time and dt>]\n";
+  usage += "\t@atoms          <atomspecifier>\n";
+  usage += "\t@selection      <atomspecifier>\n";
+  usage += "\t[@translate      (only in case com is not in between layers)]\n";
+  usage += "\t[@grid          <integer; default: 100>]\n";
+  usage += "\t@traj           <trajectory files>\n";
+  usage += "\t[@mult          <double; default: 1>]";
+  usage += "\t[@density       (in this case the density is calculated)]";
+
+
+  try {
+    Arguments args(argc, argv, knowns, usage);
+
+    InTopology it(args["topo"]);
+    System sys(it.system());
+    Boundary *pbc = BoundaryParser::boundary(sys, args);
+    Boundary::MemPtr gathmethod = args::GatherParser::parse(args);
+
+    utils::Time time(args);
+
+    AtomSpecifier atoms(sys);
+    {
+      Arguments::const_iterator to = args.upper_bound("atoms");
+      for(Arguments::const_iterator iter = args.lower_bound("atoms"); iter != to; iter++)
+        atoms.addSpecifier(iter->second);
+    }
+
+    int grid = 100;
+    {
+      Arguments::const_iterator iter = args.lower_bound("grid");
+      if(iter != args.upper_bound("grid")) {
+        istringstream in(iter->second);
+        if(!(in >> grid))
+          throw gromos::Exception(argv[0], "@grid: cutoff is not numeric.");
+      }
+    }
+
+    // selection
+    AtomSpecifier selection(sys);
+    {
+      Arguments::const_iterator to = args.upper_bound("selection");
+      for(Arguments::const_iterator iter = args.lower_bound("selection"); iter != to; iter++)
+        selection.addSpecifier(iter->second);
+    }
+
+    // Check if translate
+    bool translate = false;
+    if(args.count("translate") >=0) translate = true;
+
+    // Check if density
+    bool density = false;
+    if(args.count("density") >=0) density = true;
+
+    // mult
+    double mult = 1.0;
+    {
+      Arguments::const_iterator iter = args.lower_bound("mult");
+      if(iter != args.upper_bound("mult")) {
+        istringstream in(iter->second);
+        if(!(in >> mult))
+          throw gromos::Exception(argv[0], "@mult: mult is not numeric.");
+      }
+    }
+
+    // Calculate total mass
+    double totalMass=0.0;
+    for(int i=0; i< atoms.size(); ++i)
+      totalMass+=atoms.mass(i);
+
+    vector<double> zcoord;
+
+    double min_d;
+    double max_d;
+    Vec cm(0.0,0.0,0.0);
+    // loop over all trajectories
+    InG96 ic;
+
+    int frames = 0;
+    cout << "# DISTRIBUTION: " << endl;
+    
+    for(Arguments::const_iterator iter = args.lower_bound("traj"), to = args.upper_bound("traj");
+      iter != to; ++iter) {
+
+      // open file
+      ic.open((iter->second).c_str());
+
+      // loop over single trajectory
+      while(!ic.eof()) {
+        ic.select("ALL");
+        ic >> sys >> time;
+        // gather system
+        (*pbc.*gathmethod)();
+
+        
+        for (int i=0;i < atoms.size(); i++) {
+          cm += atoms.pos(i) * atoms.mass(i);
+        }
+        cm /= totalMass;
+
+        if (translate == true){
+          cm[2] = cm[2] - sys.box().M().abs()/2; 
+        }
+        
+        min_d = -sys.box().M().abs()/2; // before was: cm[2]-sys.box()[2]/2;
+        max_d = sys.box().M().abs()/2;
+        
+        for(int i = 0; i < selection.size(); i++) {
+          Vec & vector1 = selection.pos(i);
+          Vec dist = vector1 - pbc->nearestImage(vector1, cm, sys.box());
+          zcoord.push_back(dist[2]);
+        }
+        frames++;
+      } // while frames
+      ic.close();
+
+    } // for traj
+    gmath::Distribution dist(min_d, max_d, grid);
+
+    for(int unsigned i = 0; i < zcoord.size(); i++) {
+      dist.add(zcoord[i]);
+    }
+
+    double binwidth = (max_d - min_d)/grid;
+    
+    if(density == false) {
+      for(int k = 0; k < grid; k++) {
+        double r = dist.value(k);
+        cout << r << "\t" << double(dist[k]) * mult / (binwidth * dist.nVal()) << endl;
+      }
+    }
+    double volume = sys.box().K().abs() * sys.box().L().abs() * sys.box().M().abs();
+    if(density == true) {
+      for(int k = 0; k < grid; k++) {
+        double r = dist.value(k);
+        cout << r << setw(15) << double(dist[k]) * mult / (volume * binwidth * frames) << endl;
+      }
+    }
+    
+  } catch(const gromos::Exception &e) {
+    cerr << e.what() << endl;
+    exit(1);
+  }
+  return 0;
+}
+
+
