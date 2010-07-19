ded75c1ab647fb7bd73cf2ee5e8b65f62eb2ba1a
Dongqi Wang <dwang@ihep.ac.cn>
2010-07-19 14:55:33 +0000

    mac_fix

---
+++ gromos++/src/utils/groTime.cc
@@ -0,0 +1,61 @@
+/**
+ * @file groTime.cc
+ * implements utils::Time
+ */
+
+#include <map>
+
+
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+
+#include "../args/Arguments.h"
+#include "groTime.h"
+
+namespace utils {
+  Time::Time(const args::Arguments & args) :
+  d_t0(0.0), d_dt(1.0), d_current_time(0.0), d_read(true), d_do_timeseries(false) {
+    // get the time command line argument
+    if (args.count("time") != -1)
+      doSeries() = true;
+    
+    args::Arguments::const_iterator iter = args.lower_bound("time");
+    if (iter != args.upper_bound("time")) {
+      // argument supplied: do not read
+      read() = false;
+      std::istringstream in(iter->second);
+      if (!(in >> start_time()))
+        throw gromos::Exception("time", "@time: starting time is not numeric.");
+      ++iter;
+    }
+    if (iter != args.upper_bound("time")) {
+      std::istringstream in(iter->second);
+      if (!(in >> dt()))
+        throw gromos::Exception("time", "@time: timestep is not numeric.");
+    }
+
+    if (read() == false) {
+      // the first frame has time start_time(). so when we call the operator
+      // to read the time it will be increased to start_time() + dt(). That's
+      // why we have to start at start_time() - dt()
+      time() = start_time() - dt();
+    }
+  }
+
+  void Time::print(std::ostream & out) const {
+    // write the time
+    out.setf(std::ios::fixed, std::ios::floatfield);
+    out << std::setw(15) << std::setprecision(9) << time();
+  }
+  
+  std::ostream & operator<<(std::ostream &os, Time const & t) {
+    t.print(os);
+    return os;
+  }
+}
+
+
+
+
---
+++ gromos++/src/utils/groTime.h
@@ -0,0 +1,145 @@
+/**
+ * @file groTime.h
+ * io of TIMESTEP and \@time
+ */
+
+#ifndef INCLUDED_UTILS_TIME
+#define INCLUDED_UTILS_TIME
+
+#include <iostream>
+
+namespace args {
+  class Arguments;
+}
+
+namespace gio {
+  class InG96;
+}
+
+namespace utils{
+  /**
+   * Class Time
+   * A class to handle trajectory times. It takes care of the \c \@time command
+   * line argument. There are the following options:
+   * - no \c \@time argument:
+   *  - For programs writing a time series: read the time from the trajectory
+   *  - For programs not writing a time series by default: do not write a time
+   *    series.
+   * - \c \@time argument:
+   *  - For programs writing a time series: read the time from the trajectory.
+   *  - For programs not writing a time series by default: write a time series
+   *    and read the time from the trajectory.
+   * - \c \@time \c time \c dt argument: Same behaviour as with the \c \@time
+   *   argument but the time is not read from the trajectory but calculated when
+   *   a frame is read.
+   *
+   * @class Time
+   * @author A. Eichenberger, N. Schmid
+   * @ingroup utils
+   */
+  class Time{
+  public:
+    /**
+     * Constructor
+     * @param args the command line arguments
+     */
+    Time(const args::Arguments & args);
+    
+    /**
+     * get the current time
+     */
+    double time() const {
+      return d_current_time;
+    }
+    /**
+     * accessor to time
+     */
+    double & time() {
+      return d_current_time;
+    }
+    
+    /**
+     * accessor to timestep
+     */
+    double dt() const {
+      return d_dt;
+    }
+    /**
+     * accessor to timestep
+     */
+    double & dt() {
+      return d_dt;
+    }
+    
+    /**
+     * accessor to starting time
+     */
+    double start_time() const {
+      return d_t0;
+    }
+    /**
+     * accessor to starting time
+     */
+    double & start_time() {
+      return d_t0;
+    }
+    /**
+     * accessor to read
+     */
+    bool & read() {
+      return d_read;
+    }
+    /**
+     * accessor to read
+     */
+    bool read() const {
+      return d_read;
+    }
+    
+    /**
+     * accessor to time series
+     */
+    bool & doSeries() {
+      return d_do_timeseries;
+    }
+    /**
+     * accessor to time series
+     */
+    bool doSeries() const {
+      return d_do_timeseries;
+    }
+    
+    /**
+     * print the time to a stream
+     * @param out the stream
+     */
+    void print(std::ostream & out) const;
+    
+    /**
+     * Prints the time to a stream
+     */
+    friend std::ostream & operator<<(std::ostream &os, Time const & t);
+  protected:
+    /**
+     * the time argument t0
+     */
+    double d_t0;
+    /**
+     * the time argument dt
+     */
+    double d_dt;
+    /**
+     * the current time
+     */
+    double d_current_time;
+    /**
+     * read trajectory
+     */
+    bool d_read;
+    /**
+     * do a time series: \@time is given but maybe without parameters
+     */
+    bool d_do_timeseries;
+  };
+}
+#endif
