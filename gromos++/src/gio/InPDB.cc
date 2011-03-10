5666e47989250186b7c021ba6253a8e2323d007d
Andreas Eichenberger <a.eichenberger@fake.com>
2011-03-10 15:03:51 +0000

    makes the InPDB class reading the right positions of the PDB file

--- gromos++/src/gio/InPDB.cc
+++ gromos++/src/gio/InPDB.cc
@@ -5,18 +5,57 @@
 #include <sstream>
 #include <cassert>
 #include <iostream>
-
+#include <bits/basic_string.h>
 
 #include "../gromos/Exception.h"
 #include "../gmath/Vec.h"
 #include "InPDB.h"
 
+// the pdb file format definitions (positions of data information)
+// for more information see:
+// http://www.rcsb.org/pdb/docs/format/pdbguide2.2/guide2.2_frame.html
+#define TYPE_POS 0
+#define TYPE_LEN 6
+#define TYPE substr(TYPE_POS,TYPE_LEN)
+#define SERIAL_POS 7
+#define SERIAL_LEN 4
+#define SERIAL substr(SERIAL_POS,SERIAL_LEN)
+#define ATOMNAME_POS 13
+#define ATOMNAME_LEN 4
+#define ATOMNAME substr(ATOMNAME_POS, ATOMNAME_LEN)
+#define RESNAME_POS 17
+#define RESNAME_LEN 4
+#define RESNAME substr(RESNAME_POS, RESNAME_LEN)
+#define ALTLOC_POS 21
+#define ALTLOC_LEN 1
+#define ALTLOC substr(ALTLOC_POS,ALTLOC_LEN)
+#define RESNUM_POS 22
+#define RESNUM_LEN 4
+#define RESNUM substr(RESNUM_POS, RESNUM_LEN)
+#define COORDX_POS 30
+#define COORDX_LEN 8
+#define COORDX substr(COORDX_POS, COORDX_LEN)
+#define COORDY_POS 38
+#define COORDY_LEN 8
+#define COORDY substr(COORDY_POS, COORDY_LEN)
+#define COORDZ_POS 46
+#define COORDZ_LEN 8
+#define COORDZ substr(COORDZ_POS, COORDZ_LEN)
+#define BFACTOR_POS 60
+#define BFACTOR_LEN 6
+#define BFACTOR substr(BFACTOR_POS,BFACTOR_LEN)
+#define OCCUPANCY_POS 54
+#define OCCUPANCY_LEN 6
+#define OCCUPANCY substr(OCCUPANCY_POS,OCCUPANCY_LEN)
+#define ELEMENT_POS 75
+#define ELEMENT_LEN 3
+#define ELEMENT substr(ELEMENT_POS,ELEMENT_LEN)
 
 using namespace std;
 
 namespace gio {
 
-    /**
+  /**
    * Class InPDB_ is the implementations class of the class PDB. It holds
    * all the member data so the PDB class needs nothing more than a pointer
    * to this class for its data.
@@ -92,6 +131,7 @@ namespace gio {
   };
 
   // Constructor
+
   InPDB::InPDB(const std::string &filename, bool readATOM, bool readHETATOM) {
     d_this = new InPDB_i;
     d_this->filemane = filename;
@@ -138,61 +178,83 @@ namespace gio {
     int res = -1;
     while (!fin.eof()) {
       getline(fin, line);
-      ssline << line;
-      string type;
-      ssline >> type;
+      string type = "";
+      if(line.size() > TYPE_POS) {
+        ssline << line.TYPE << endl;
+        ssline >> type;
+      }
+      ssline.clear();
+      ssline.str("");
       if ((type == "ATOM" && d_this->readATOM) ||
               (type == "HETATOM" && d_this->readHETATOM)) {
-
-        int serial;
-        string atom;
-        string altLoc;
-        string resName;
-        int seqNo;
-        double x;
-        double y;
-        double z;
-        double occupancy;
-        double tempFactor;
-        string element;
-
-        ssline >> serial >> atom >> resName >> altLoc >> seqNo
-                >> x >> y >> z >> occupancy >> tempFactor >> element;
-
-        // error message if failure while reading
-        if (ssline.bad() || ssline.fail()) {
+        cerr << "1: line.size() = " << line.size() << ", BFACTOR_POS = " << BFACTOR_POS << endl;
+        if(line.size() > BFACTOR_POS) { // at least GROMOS PDB file format
+          ssline << line.SERIAL << endl;
+          ssline << line.ATOMNAME << endl;
+          ssline << line.RESNAME << endl;
+          ssline << line.ALTLOC << endl;
+          ssline << line.RESNUM << endl;
+          ssline << line.COORDX << endl;
+          ssline << line.COORDY << endl;
+          ssline << line.COORDZ << endl;
+          ssline << line.OCCUPANCY << endl;
+          ssline << line.BFACTOR << endl;
+          if(line.size() > ELEMENT_POS) { // officiela PDB file format
+            ssline << line.ELEMENT << endl;
+          }
+          int serial;
+          string atom;
+          string altLoc;
+          string resName;
+          int seqNo;
+          double x;
+          double y;
+          double z;
+          double occupancy;
+          double tempFactor;
+          string element;
+          // try to convert the read calues to the correct file type
+          ssline >> serial >> atom >> resName >> altLoc >> seqNo
+                    >> x >> y >> z >> occupancy >> tempFactor;
+          // there is an additional column in the official PDF file format
+          if(line.size() > ELEMENT_POS) {
+            ssline >> element;
+          }
+          // error message in case of the conversion failed...
+          if (ssline.bad() || ssline.fail()) {
+            stringstream msg;
+            msg << "bad line in PDB file:\n" << line;
+            throw gromos::Exception("InPDB.cc", msg.str());
+          }
+          // memorize the read variables
+          d_this->types.push_back(type);
+          d_this->serials.push_back(serial);
+          d_this->atoms.push_back(atom);
+          d_this->altLocs.push_back(altLoc);
+          d_this->resNames.push_back(resName);
+          d_this->seqNos.push_back(seqNo);
+          d_this->X.push_back(x);
+          d_this->Y.push_back(y);
+          d_this->Z.push_back(z);
+          d_this->occupancies.push_back(occupancy);
+          d_this->tempFactors.push_back(tempFactor);
+          if (line.size() > ELEMENT_POS) {
+            d_this->elements.push_back(element);
+          }
+          // add the residue to the sequence (in case of a new residue)
+          if (res != seqNo) {
+            res = seqNo;
+            d_this->resSeq.push_back(resName);
+          }
+          ssline.clear();
+          ssline.str("");
+        } else {
           stringstream msg;
-          msg << "bad line in " << d_this->filemane << " while reading (" << type << ")";
-          throw InPDB::Exception(msg.str());
+          msg << "bad line in PDB file:\n" << line;
+          throw gromos::Exception("InPDB.cc", msg.str());
         }
-
-        // memorize the read variables
-        d_this->types.push_back(type);
-        d_this->serials.push_back(serial);
-        d_this->atoms.push_back(atom);
-        d_this->altLocs.push_back(altLoc);
-        d_this->resNames.push_back(resName);
-        d_this->seqNos.push_back(seqNo);
-        d_this->X.push_back(x);
-        d_this->Y.push_back(y);
-        d_this->Z.push_back(z);
-        d_this->occupancies.push_back(occupancy);
-        d_this->tempFactors.push_back(tempFactor);
-        d_this->elements.push_back(element);
-
-        // add the residue to the sequence (in case of a new residue)
-        if (res != seqNo) {
-          res = seqNo;
-          d_this->resSeq.push_back(resName);
-        }
-
       }
-
-      // clean the stringstream
-      ssline.str("");
-      ssline.clear();
-    }
-
+    } // while
     // close the PDB file
     fin.close();
 
