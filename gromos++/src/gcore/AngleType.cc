d37d79a5b1e495af6e78979f3bb31dab0077951d
Andreas Eichenberger <a.eichenberger@fake.com>
2011-05-17 20:51:51 +0000

    INTERNAL fix of ticket #190 - eandreas

--- gromos++/src/gcore/AngleType.cc
+++ gromos++/src/gcore/AngleType.cc
@@ -3,61 +3,68 @@
 #include <cmath>
 #include "AngleType.h"
 #include <new>
+#include <sstream>
 #include "../gromos/Exception.h"
 #include "../args/Arguments.h"
 #include "../gmath/Physics.h"
 
+using namespace std;
 using gcore::AngleType;
 
-AngleType &AngleType::operator=(const AngleType &b){
-  if(this!=&b){
+AngleType &AngleType::operator=(const AngleType &b) {
+  if (this != &b) {
     this->~AngleType();
     new(this) AngleType(b);
   }
   return *this;
 }
 
-AngleType::AngleType(int c, double fc, double l) : d_code(c), d_t0(l),d_fc(fc),
-        d_afc(0.0) {
-  // we only got the harmonic-in-cosine force constant and have to calculate the
-  // harmonic-in-angle force constant.
-  // we do this as discribed in volume 3
-  
+AngleType::AngleType(int c, double fc, double l) : d_code(c), d_t0(l), d_fc(fc),
+d_afc(0.0) {
+
   // first let's make sure it is not simply zero.
   if (d_fc == 0.0) {
     return;
   }
-  
+
   double t_0 = d_t0 * gmath::physConst.get_degree2radian();
-  
+
   // this is kT at 300K - a reasonable choice for biomolecular simulations.
   const double kT = gmath::physConst.get_boltzmann() * 300.0;
-  
-  const double X = cos(t_0) * cos(t_0);
-  const double Y = (d_fc - kT) / d_fc;
-  const double Z = X - sin(t_0) * sin(t_0);
-  
-  const double under_sqrt = X*X - Y*Z;
-  if (under_sqrt < 0 || Z == 0.0)
-    throw gromos::Exception("AngleType", "Cannot automatically convert "
-            "force constant: discriminant negative.");
 
-  const double cos_angle = (X - sqrt(under_sqrt)) / Z;
-  if (cos_angle < -1.0 || cos_angle > 1.0)
-    throw gromos::Exception("AngleType", "Cannot automatically convert "
-            "force constant: cosine out of range.");
+  // the two solutions for theta where V_harm(theta) = V_cos-harm(theta)
+  if(d_fc < 0) {
+    stringstream msg;
+    msg << "negative cosine-harmonic force constant k = " << d_fc
+            << " does not make sense";
+    throw gromos::Exception("AngleType", msg.str());
+  }
   
-  const double angle = acos(cos_angle);
-  if (angle == 0.0)
-    throw gromos::Exception("AngleType", "Cannot automatically convert "
-            "force constant: angle is zero.");
+  double costheta1 = cos(t_0) + sqrt(kT / d_fc);
+  double costheta2 = cos(t_0) - sqrt(kT / d_fc);
   
-  d_afc = kT / (angle * angle);
+  double theta1, theta2;
+  
+  if(costheta1 <= 1 && costheta1 >= -1) {
+    theta1 = acos(costheta1);
+    if(costheta2 <= 1 && costheta2 >= -1) {
+      theta2 = acos(costheta2);
+    } else {
+      theta2 = 2 * t_0 - theta1;
+    }
+  } else {
+    theta2 = acos(costheta2);
+    theta1 = 2 * t_0 - theta2;
+  }
+  
+  double term1 = (theta1 - t_0) * (theta1 - t_0);
+  double term2 = (theta2 - t_0) * (theta2 - t_0);
+  
+  d_afc = 2 * kT / (term1 + term2);
   
   // do a warning as this may be a bit tricky
   if (!args::Arguments::outG96) {
     std::cerr << "Warning for bond angle type " << d_code + 1 << ": Harmonic force "
             "constant was calculated at 300K." << std::endl;
-    std::cerr << "WARNING: check the calculated value before running the simulation!!!\n";
   }
 }
