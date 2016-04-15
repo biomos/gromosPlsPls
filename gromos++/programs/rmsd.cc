a1412492b52176b24af3a05e171b7a6d7b7ce2b0
Chris Oostenbrink <chris.oostenbrink@boku.ac.at>
2016-04-15 19:08:07 +0000

    allow for the use of a reference structure with different topology--MariaP

--- gromos++/programs/rmsd.cc
+++ gromos++/programs/rmsd.cc
@@ -17,6 +17,10 @@
  * to the rmsd calculation. The fit
  * can be performed using a different set of atoms than the calculation of the 
  * rmsd. If no fit is required "no" should be given.
+ * Optionally a separate topology can be given for the reference. The atom 
+ * selections then have to be chosen such that equivalent atoms in the system and 
+ * reference system are selected and that they are in the same order. To be able to 
+ * check this, option \@printatoms will print the selected atoms for each topology.
  *
  * <b>arguments:</b>
  * <table border=0 cellpadding=0>
@@ -25,7 +29,9 @@
  * <tr><td> \@time</td><td>&lt;@ref utils::Time "time and dt"&gt; </td></tr>
  * <tr><td> \@atomsrmsd</td><td>&lt;@ref AtomSpecifier "atoms" to consider for rmsd&gt; </td></tr>
  * <tr><td> [\@atomsfit</td><td>&lt;@ref Atomspecifier "atoms" to consider for fit&gt;] </td></tr>
+ * <tr><td> [\@reftopo</td><td>&lt;molecular topology file for the reference&gt;] </td></tr>
  * <tr><td> [\@ref</td><td>&lt;reference coordinates (if absent, the first frame of \@traj is reference)&gt;] </td></tr>
+ * <tr><td> [\@printatoms</td><td>print list of selected atoms] </td></tr>
  * <tr><td> \@traj</td><td>&lt;trajectory files&gt; </td></tr>
  * </table>
  *
@@ -34,7 +40,9 @@
  * @verbatim
   ditrans
     @topo       ex.top
-    @pbc        r
+    @reftopo    ref.top
+    @pbc        r cog
+    @refpbc     r cog
     @time       0 0.1
     @atomsrmsd  1:CA
     @atomsfit   1:CA,C,N
@@ -77,6 +85,8 @@
 #include "../src/utils/groTime.h"
 #include "../src/gmath/Vec.h"
 #include "../src/utils/PropertyContainer.h"
+#include "../src/gmath/Matrix.h"
+#include "../src/fit/FastRotationalFit.h"
 
 
 using namespace gcore;
@@ -88,10 +98,24 @@ using namespace fit;
 using namespace std;
 using namespace gmath;
 
+double rmsdproperty(const utils::PropertyContainer &prop_ref, 
+                             const utils::PropertyContainer &prop_sys ){
+
+  double rmsd2=0;
+
+  for(unsigned int i=0; i < prop_ref.size(); i++){
+    
+    utils::Value res = abs2(prop_ref[i]->calc() - prop_sys[i]->calc());
+    rmsd2 += res.scalar();
+  }
+  
+  return utils::sqrt(rmsd2/prop_ref.size());
+}
+
 int main(int argc, char **argv){
   Argument_List knowns; 
   knowns << "topo" << "traj" << "atomsfit" << "atomsrmsd" << "prop" << "pbc" << "ref"
-         << "time" << "debug" << "fit";
+         << "time" << "debug" << "fit" << "reftopo" << "refpbc" << "printatoms";
 
   string usage = "# " + string(argv[0]);
   usage += "\n\t@topo       <molecular topology file>\n";
@@ -99,8 +123,11 @@ int main(int argc, char **argv){
   usage += "\t@time       <time and dt>\n";
   usage += "\t@atomsrmsd  <atoms to consider for rmsd>\n";
   usage += "\t[@atomsfit  <atoms to consider for fit>]\n";
+  usage += "\t[@reftopo       <molecular topology file for the reference>\n";
+  usage += "\t[@refpbc       <pbc and gather method for the reference if @reftopo is given, default: box from @ref, no gathering>\n";
   usage += "\t[@prop      <properties>\n";
   usage += "\t[@ref        <reference coordinates (if absent, the first frame of @traj is reference)>]\n";
+  usage += "\t[@printatoms     < print list of selected atoms >]\n";
   usage += "\t@traj       <trajectory files>\n";
 
 
@@ -114,16 +141,28 @@ int main(int argc, char **argv){
     // get simulation time
     Time time(args);
     // read topology
-    InTopology it(args["topo"]);
-    System refSys(it.system());
-    //System sys(refSys);
+    InTopology it(args["topo"]);    
+
     System sys(it.system());
- 
-    // Parse boundary conditions
-    Boundary *pbc = BoundaryParser::boundary(refSys, args);
-    // GatherParser
-    Boundary::MemPtr gathmethod = args::GatherParser::parse(sys,refSys,args);
-   
+    System refSys;
+    if (args.count("reftopo")>0) {
+      if (args.count("ref")<=0) {
+      throw gromos::Exception("rmsd",
+                    "reftopo only makes sense when @ref is given");        
+      }
+      cerr << "# Be careful: using @reftopo will only work correctly if the atomspecifiers\n"
+           << "# in atomsrmsd or atomsfit specify the equivalent atoms in \n"
+           << "# both topologies in the same order!\n#\n";
+      InTopology refit(args["reftopo"]);
+      refSys=refit.system();
+    } else {
+      refSys=it.system();
+    }   
+     
+    //print selected atoms or not
+    bool printatoms=false;
+    if (args.count("printatoms") >= 0) printatoms=true;
+    
     // read reference coordinates...
     InG96 ic;
     if(args.count("ref")>0)
@@ -135,7 +174,23 @@ int main(int argc, char **argv){
     ic.select("ALL");
     ic >> refSys;
     ic.close();
-    //cout << " ref mol " << refSys.numMolecules() << " solv " << refSys.sol(0).numPos() << endl;
+    
+    Boundary *refpbc = BoundaryParser::boundary(refSys);
+    Boundary::MemPtr refgathmethod=&Boundary::nogather;
+    if(args.count("refpbc")>0) {
+       // Parse boundary conditions and gathermethod for reference
+       refpbc = BoundaryParser::boundary(refSys, args, "refpbc");
+       refgathmethod= args::GatherParser::parse(refSys,refSys,args, "refpbc");
+       (*refpbc.*refgathmethod)();
+       // if no reference topology has been given, use normal @pbc flag
+    } else if (args.count("reftopo")<=0) {
+       refpbc = BoundaryParser::boundary(refSys, args);
+       refgathmethod= args::GatherParser::parse(refSys,refSys,args);
+       (*refpbc.*refgathmethod)();  
+    } else {
+      cerr << "# Warning: @reftopo but no @refpbc : reference will not be gathered\n";
+    }
+    
 
     int debug=0;
     if(args.count("debug")>0)
@@ -156,30 +211,23 @@ int main(int argc, char **argv){
                               "Unable to read POSITION(RED) block from "
 			      "reference positions file.");
 
-    (*pbc.*gathmethod)();
-
-    delete pbc;
 
     // System for calculation
-    Reference refrmsd(&refSys);
-    AtomSpecifier fitatoms(refSys);
     AtomSpecifier rmsdatoms(sys);
+    AtomSpecifier rmsdatomsref(refSys);
+    AtomSpecifier fitatoms(sys);
+    AtomSpecifier fitatomsref(refSys);
 
     //get rmsd atoms
-    if(args.count("atomsrmsd")>0){
-    {
+    if(args.count("atomsrmsd")>0){    
        Arguments::const_iterator iter = args.lower_bound("atomsrmsd");
        Arguments::const_iterator to = args.upper_bound("atomsrmsd");
 
        for(;iter!=to;iter++){
         string spec=iter->second.c_str();
         rmsdatoms.addSpecifier(spec);
-       }
-    }  
-    if(rmsdatoms.size()==0)
-      throw gromos::Exception("rmsd", "No rmsd-atoms specified!");
-    
-    refrmsd.addAtomSpecifier(rmsdatoms);
+        rmsdatomsref.addSpecifier(spec);
+       }  
     }
 
     //try for fit atoms
@@ -189,26 +237,45 @@ int main(int argc, char **argv){
 
       for(;iter!=to;iter++){
         string spec=iter->second.c_str();
+        fitatomsref.addSpecifier(spec);
         fitatoms.addSpecifier(spec);
       }
     } else {
       cout << "# @atomsrmsd atoms are taken for fit." << endl;
-      const vector<string> & spec = rmsdatoms.toString();
-
-      for(vector<string>::const_iterator it = spec.begin(), to = spec.end();
-              it != to; ++it) {
-        fitatoms.addSpecifier(*it);
-      }
+      fitatoms=rmsdatoms;
+      fitatomsref=rmsdatomsref;
     }
+    
+    // optionally print atomspecs to check them
+    int maxsize=0;
+    if (fitatoms.size() > rmsdatoms.size()) maxsize=fitatoms.size();
+    else maxsize=rmsdatoms.size();
+    if (printatoms) {
+        for (int i=0; i < maxsize; i++) {
+              cerr << "# " ;
+            if (i<fitatoms.size()) {
+              cerr << fitatoms.toString(i) << " "<< fitatoms.mol(i)+1 <<":"<< fitatoms.resname(i) << fitatoms.resnum(i)+1<<":" << fitatoms.name(i);
+            } 
+            cerr << "\t-\t";
+            if (i<fitatomsref.size()) {
+              cerr << fitatomsref.toString(i) << " "<< fitatomsref.mol(i)+1 <<":"<< fitatomsref.resname(i) << fitatomsref.resnum(i)+1<<":" << fitatomsref.name(i);
+            }
+            cerr <<  endl;
+        }
+    }
+    
+    if (fitatomsref.size() != fitatoms.size() || rmsdatomsref.size() != rmsdatoms.size())
+      throw gromos::Exception("rmsd", "Number of atoms specified by atomsfit or atomsrmsd is not the same in topo and reftopo! \n@printatoms can help you find the problem.");
 
-        // Parse boundary conditions for sys
-    pbc = BoundaryParser::boundary(sys, args);
+    // Parse boundary conditions for sys
+    Boundary *pbc = BoundaryParser::boundary(sys, args);
+    // the second parse argument is only used for generating the primlist (for list gathering)
+    // also  in the reference; since I'm gathering the reference separately
+    // I don't need it
+    Boundary::MemPtr gathmethod = args::GatherParser::parse(sys,sys,args);
 
     // Property container also for reference system
-
- 
-
-     PropertyContainer prop_ref(refSys, pbc);
+     PropertyContainer prop_ref(refSys, refpbc);
      PropertyContainer prop_sys(sys, pbc);
     {
       std::string prop;
@@ -226,26 +293,13 @@ int main(int argc, char **argv){
     }
 
 
+   FastRotationalFit frf;
    if(args.count("atomsrmsd") < 0 && args.count("prop") < 0){
     throw gromos::Exception("rmsd", "No rmsd atoms or property specified!");}
-   
-
-    //Vec cog=PositionUtils::cog(refSys, reffit);
-
-    RotationalFit * rf = NULL;
-    if (fitatoms.size()) {
-      Reference * ref = new Reference(&refSys);
-      ref->addAtomSpecifier(fitatoms);
-      rf = new RotationalFit(ref);
-    }
-    
-    Rmsd rmsd(&refrmsd);
-    rmsd.addproperty(&prop_ref, &prop_sys);
 
     int numFrames = 0;
 
-    //cout << rmsd.rmsd(refSys) << endl;
-    
+   
     // loop over all trajectories
     for(Arguments::const_iterator iter=args.lower_bound("traj");
 	iter!=args.upper_bound("traj"); ++iter){
@@ -253,39 +307,64 @@ int main(int argc, char **argv){
       
       // loop over all frames
       while(!ic.eof()){
+        Matrix rot(3,3,0.0);
+        for(int i=0;i<3;++i){ // initialize as identity matrix
+          rot(i,i)=1.0;
+        }
           
           numFrames++;
           ic.select("ALL");
-	ic >> sys >> time;
+	      ic >> sys >> time;
         if(!sys.hasPos)
           throw gromos::Exception("rmsd",
                              "Unable to read POSITION(RED) block from "
                               "trajectory file.");
 	
-	(*pbc.*gathmethod)();
+	    (*pbc.*gathmethod)();
 
-        if (fitatoms.size())
-          rf->fit(&sys);
-                
-        double r = rmsd.rmsd(sys);
-        double rprop = rmsd.rmsdproperty(sys);
+        // using FastRotationalFit::fit and rmsd methods
+        // they take position vectors as input
+        // shift to cog before rotational fit
+        Vec cogfitref (0,0,0);
+        Vec cogfitsys (0,0,0);
+        if (fitatomsref.size()){
+          for(int i=0;i<fitatomsref.size();++i) {
+            cogfitref+=fitatomsref.pos(i);
+            cogfitsys+=fitatoms.pos(i);
+          }
+          cogfitref/=fitatomsref.size();
+          cogfitsys/=fitatomsref.size();
+        }
+        
+        std::vector<gmath::Vec> fitref;
+        std::vector<gmath::Vec> fitsys;
+        std::vector<gmath::Vec> rmsdref;
+        std::vector<gmath::Vec> rmsdsys;
+        if (fitatomsref.size()){
+          for(int i=0;i<fitatomsref.size();++i) {
+            fitref.push_back(fitatomsref.pos(i)-cogfitref);
+            fitsys.push_back(fitatoms.pos(i)-cogfitsys);
+          }
+          for(int i=0;i<rmsdatomsref.size();++i) {
+            rmsdref.push_back(rmsdatomsref.pos(i)-cogfitref);
+            rmsdsys.push_back(rmsdatoms.pos(i)-cogfitsys);
+          }
+          frf.fit(rot, fitref, fitsys);
+        }
 
+        double r = frf.rmsd(rot, rmsdref, rmsdsys);
+        double rprop = rmsdproperty(prop_ref, prop_sys);
 
-
-
-//	cout.precision(2);
-//	cout << time;
-//	cout.precision(5);
         if (args.count("atomsrmsd") > 0 && args.count("prop") > 0) {
           cout.precision(2);
           cout << time;
           cout.precision(9);
-          cout << setw(15) << r << setw(15) << rprop << endl;
+          cout << setw(15) << r  << setw(15) << rprop << endl;
         } else if(args.count("atomsrmsd") > 0){
           cout.precision(2);
           cout << time;
           cout.precision(5);
-          cout << setw(10) << r << endl;
+          cout << setw(10) << r  << endl;
         } else if(args.count("prop") > 0){
           cout.precision(2);
           cout << time;
@@ -296,10 +375,10 @@ int main(int argc, char **argv){
       ic.close();
     }
 
-    if (rf != NULL) {
-      delete rf->getReference();
-      delete rf;
-    }
+    //if (rf != NULL) {
+     // delete rf->getReference();
+      //delete rf;
+    //}
     
   }
   catch (const gromos::Exception &e){
