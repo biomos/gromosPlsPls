4d85c5b75912c18941dc7b3f00d70f7f61b76538
dpetrov <drazen.petrov@boku.ac.at>
2023-10-20 22:49:23 +0200

    GPLv2 added to all relevant files

--- gromos++/examples/fit_JJ.py
+++ gromos++/examples/fit_JJ.py
@@ -1,4 +1,24 @@
 """
+This file is part of GROMOS.
+
+Copyright (c) 2011, 2012, 2016, 2018, 2021, 2023 Biomos b.v.
+See <https://www.gromos.net> for details.
+
+GROMOS is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+"""
 author dp
 
 fit_JJ.py script fits the JJ.out data from epsilon program as described in [Schroeder & Steinhauser, J. Chem. Phys. 132, 244109 (2010); doi. 10.1063/1.3432620]
